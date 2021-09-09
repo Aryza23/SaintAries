3739cd01931301dc5ae680fcc2cd546ad7794e42
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 17:07:45 +0700

    Add files via upload

--- aries/modules/connection.py
+++ aries/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import zerotwobot.modules.sql.connection_sql as sql
-from zerotwobot import dispatcher, DRAGONS, DEV_USERS
-from zerotwobot.modules.helper_funcs import chat_status
-from zerotwobot.modules.helper_funcs.alternate import send_message, typing_action
+import aries.modules.sql.connection_sql as sql
+from aries import dispatcher, DRAGONS, DEV_USERS
+from aries.modules.helper_funcs import chat_status
+from aries.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
