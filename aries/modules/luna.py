43fbc7b56c83585799f3aa1987cfc6900d8de376
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 23:31:10 +0700

    Update luna.py

--- aries/modules/luna.py
+++ aries/modules/luna.py
@@ -6,6 +6,7 @@ from aiohttp import ClientSession
 from pyrogram import Client, filters, idle
 from Python_ARQ import ARQ
 from aries.modules import sample_config
+from aries.modules.sample_config import bot_token
 
 is_config = os.path.exists("config.py")
 
