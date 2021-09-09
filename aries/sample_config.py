bb56643575a9c382a8245cfd33a33d292468566e
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 19:52:23 +0700

    Update sample_config.py

--- aries/sample_config.py
+++ aries/sample_config.py
@@ -1,6 +1,7 @@
-
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
 bot_token = "1914584978:AAEuz9eFFiKop48U2w-2pDQtaNQ61RtbieY"
 LANGUAGE = "id"
+owner_id = 1192108540
+bot_id = 1914584978
 ARQ_API_KEY = "CQTLUO-UJYJVQ-AHTLCB-NASYBF-ARQ"
