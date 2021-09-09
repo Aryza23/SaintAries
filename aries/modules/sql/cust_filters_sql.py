45c824a80494b4568d80ae350aa5f429338d5a5b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 15:18:41 +0700

    Update cust_filters_sql.py

--- aries/modules/sql/cust_filters_sql.py
+++ aries/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from zerotwobot.modules.helper_funcs.msg_types import Types
-from zerotwobot.modules.sql import BASE, SESSION
+from aries.modules.helper_funcs.msg_types import Types
+from aries.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
