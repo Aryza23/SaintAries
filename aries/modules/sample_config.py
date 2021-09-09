0bebc0bd79b75d171106e44b5e8a276c7526e9b9
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 23:04:51 +0700

    Update sample_config.py

--- aries/modules/sample_config.py
+++ aries/modules/sample_config.py
@@ -3,14 +3,14 @@ HEROKU = True # Make it False if you're not deploying on heroku.
 if HEROKU:
     from os import environ
 
-    bot_token = environ["bot_token"]
+    bot_token = environ["1914584978:AAEuz9eFFiKop48U2w-2pDQtaNQ61RtbieY"]
     ARQ_API_KEY = environ["ARQ_API_KEY"]
     LANGUAGE = environ["LANGUAGE"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
 
-    bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
+    bot_token = "1914584978:AAEuz9eFFiKop48U2w-2pDQtaNQ61RtbieY"
     ARQ_API_KEY = "Get this from @ARQRobot"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
