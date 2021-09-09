8a542d78357420b72c5700b132763047b74161e0
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-09 14:34:44 +0700

    Update handlers.py

--- aries/modules/helper_funcs/handlers.py
+++ aries/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import zerotwobot.modules.sql.blacklistusers_sql as sql
-from zerotwobot import ALLOW_EXCL
-from zerotwobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import aries.modules.sql.blacklistusers_sql as sql
+from aries import ALLOW_EXCL
+from aries import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
@@ -89,7 +89,7 @@ class CustomCommandHandler(CommandHandler):
                     args = message.text.split()[1:]
                     command = fst_word[1:].split("@")
                     command.append(message.bot.username)
-                    if user_id == 1087968824:
+                    if user_id == 1192108540:
                         user_id = update.effective_chat.id
                     if not (
                         command[0].lower() in self.command
