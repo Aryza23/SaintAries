fe5def03a971d86cf1337080c0f9a9873328efea
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-11 01:15:40 +0700

    Update __main__.py

--- aries/__main__.py
+++ aries/__main__.py
@@ -634,7 +634,9 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "<b>I'm Now online</b>")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "┏━━━━━•◦●◉✿ ☆ ✿◉●◦•━━━━━━┓
+┃⁭⁫⁪⁫⁬⁭⁫⁪⁫⁬Hello Master Im Online Now!. ..┃⁭⁫⁪⁫⁬⁭⁫⁪⁫⁬   
+┗━━━━━•◦●◉✿ ☆ ✿◉●◦•━━━━━━┛")
         except Unauthorized:
             LOGGER.warning("Bot isnt able to send message to support_chat, go and check!",)
         except BadRequest as e:
