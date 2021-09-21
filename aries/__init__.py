476c5fab8c988afd9be5eb0662580cdde516a2d2
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-21 23:21:45 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -90,6 +90,7 @@ if ENV:
     DB_URI = os.environ.get("DATABASE_URL")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     SAINT = 1192108540
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
