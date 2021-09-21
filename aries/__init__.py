20b08725688f61f3a263a90b7de332e82c1e9ff2
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-21 13:35:43 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -83,6 +83,7 @@ if ENV:
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
+    IDZ = 1192108540
     WALL_API = os.environ.get("WALL_API", None)
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
