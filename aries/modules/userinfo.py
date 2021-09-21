620bacaddd8b26a0bbd7e41ae46a865d2604b1fa
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-21 22:09:38 +0700

    Update userinfo.py

--- aries/modules/userinfo.py
+++ aries/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/IDZEROID">?</a>]'.format(
+        text += ' [<a href="https://t.me/AriesXidzeroid/17">?</a>]'.format(
             bot.username,
         )
 
