70a1c6b8c86d2d4a69edf3c01a20a8a3bc4b4da3
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-22 11:34:49 +0700

    Update member.py

--- aries/modules/member.py
+++ aries/modules/member.py
@@ -10,7 +10,7 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("APP_ID"))
+api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("TOKEN")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
