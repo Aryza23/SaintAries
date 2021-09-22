c689c2e97ae52cac5e4ef5719b865a12b65d53d4
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-22 22:11:27 +0700

    Update tagall.py

--- aries/modules/tagall.py
+++ aries/modules/tagall.py
@@ -15,7 +15,7 @@ async def mentionall(event):
     return await event.respond("__This command can be use in groups and channels!__")
   
   admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+  async for admin in bot.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
     admins.append(admin.id)
   if not event.sender_id in admins:
     return await event.respond("🔴 YOU ARE NOT AN ADMIN IN THESE GROUP")
@@ -36,11 +36,11 @@ async def mentionall(event):
   if mode == "text_on_cmd":
     usrnum = 0
     usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
+    async for usr in bot.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
       if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await bot.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
@@ -48,11 +48,11 @@ async def mentionall(event):
   if mode == "text_on_reply":
     usrnum = 0
     usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
+    async for usr in bot.iter_participants(event.chat_id):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
       if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await bot.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
