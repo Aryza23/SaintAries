3d5865343fc225f0619afc2d9999257c20ca60d4
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-24 21:25:54 +0700

    Create truth_and_dare.py

---
+++ aries/modules/truth_and_dare.py
@@ -0,0 +1,22 @@
+import requests
+from aries.events import register
+
+
+@register(pattern="^/truth ?(.*)")
+async def _(td):
+    try:
+        kuntul = requests.get("https://api-tede.herokuapp.com/api/truth").json()
+        results = f"**{kuntul['message']}**"
+        return await td.reply(results)
+    except Exception:
+        await td.reply("`Sedang Bermasalah...`")
+
+
+@register(pattern="^/dare ?(.*)")
+async def _(dr):
+    try:
+        kuntul = requests.get("https://api-tede.herokuapp.com/api/dare").json()
+        results = f"**{kuntul['message']}**"
+        return await dr.reply(results)
+    except Exception:
+        await dr.reply("`Sedang Bermasalah...`")
