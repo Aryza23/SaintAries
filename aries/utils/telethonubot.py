b8e0c0790ce23996cabcc5af65b7c51f7a4e532f
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-25 09:31:54 +0700

    Create telethonubot.py

---
+++ aries/utils/telethonubot.py
@@ -0,0 +1,15 @@
+import sys
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from aries.conf import get_int_key, get_str_key
+
+STRING_SESSION = get_str_key("STRING_SESSION", required=True)
+API_ID = get_int_key("API_ID", required=True)
+API_HASH = get_str_key("API_HASH", required=True)
+
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+try:
+    ubot.start()
+except BaseException:
+    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
