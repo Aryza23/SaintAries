69bdc88c5a0fb00e9b48e59ddc1fd437c73f4d18
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-26 12:54:28 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -105,20 +105,6 @@ if ENV:
     CHAT_ID = int(os.environ.get("CHAT_ID", "0"))
     SESSION_STREAM = os.environ.get("SESSION_STRING", "")
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    API_ID = int(os.environ.get("API_ID", "0"))
-    API_HASH = os.environ.get("API_HASH", "")
-    LOG_ID = int(os.environ.get("LOG_ID", "0"))
-    SESSION_STREAM = os.environ.get("SESSION_STREAM", "")
-
-class Database:
-    VIDEO_CALL = {}
-    RADIO_CALL = {}
-    FFMPEG_PROCESSES = {}
 
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
