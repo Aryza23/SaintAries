62b4c17301f29390aceee320900c374ee404b98a
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-28 14:21:08 +0700

    Update aichat.py

--- aries/modules/aichat.py
+++ aries/modules/aichat.py
@@ -98,8 +98,7 @@ def aichat(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kuki-api.tk/api/Raiden/moezilla/message='+Message),
-        Kukiurl = requests.get('https://kuki-api.tk/api/botname/ownername/message='+Message),
+        kukiurl = requests.get('https://kuki-api.tk/api/Raiden/moezilla/message='+Message)
         Kukiurl = requests.get('https://kuki-api.tk/api/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
