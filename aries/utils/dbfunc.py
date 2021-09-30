6c8cab5fa533232df2a838e9531f32c8be5faa6e
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-30 09:21:50 +0700

    Update dbfunc.py

--- aries/utils/dbfunc.py
+++ aries/utils/dbfunc.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Union
 
 
 coupledb = db.couple
-
+karmadb = db.karma
 
 # Couple Chooser
 
@@ -27,7 +27,7 @@ async def get_couple(chat_id: int, date: str):
 async def save_couple(chat_id: int, date: str, couple: dict):
     lovers = await _get_lovers(chat_id)
     lovers[date] = couple
-    await coupledb.update_one(
+    coupledb.update_one(
         {"chat_id": chat_id},
         {
             "$set": {
@@ -36,3 +36,74 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         },
         upsert=True
     )
+
+
+# Karma functions
+
+
+async def get_karmas_count() -> dict:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return {}
+    chats_count = 0
+    karmas_count = 0
+    for chat in await chats.to_list(length=1000000):
+        for i in chat['karma']:
+            karmas_count += chat['karma'][i]['karma']
+        chats_count += 1
+    return {
+        "chats_count": chats_count,
+        "karmas_count": karmas_count
+    }
+
+
+async def get_karmas(chat_id: int) -> Dict[str, int]:
+    karma = karmadb.find_one({"chat_id": chat_id})
+    if karma:
+        karma = karma['karma']
+    else:
+        karma = {}
+    return karma
+
+
+async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    if name in karmas:
+        return karmas[name]
+
+
+async def update_karma(chat_id: int, name: str, karma: dict):
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    karmas[name] = karma
+    karmadb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "karma": karmas
+            }
+        },
+        upsert=True
+    )
+
+
+# Alpha integer 
+
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    user_id = ""
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
+        user_id += str(index)
+    user_id = int(user_id)
+    return user_id
