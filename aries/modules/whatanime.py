1181ff8c52f31dc22b6db5de94627d0409b3b8e7
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-01 19:01:18 +0700

    Update whatanime.py

--- aries/modules/whatanime.py
+++ aries/modules/whatanime.py
@@ -16,7 +16,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import is_image, is_video
 
-from KanekiRobot.events import register as kaneki
+from aries.events import register as kaneki
 
 
 
