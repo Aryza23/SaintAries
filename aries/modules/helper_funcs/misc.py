ce57f648974cbc337adc4d391bbb13f59fc82c8c
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-03 23:22:50 +0700

    Update misc.py

--- aries/modules/helper_funcs/misc.py
+++ aries/modules/helper_funcs/misc.py
@@ -60,12 +60,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 6)
+    max_num_pages = ceil(len(pairs) / 3)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 4:
-        pairs = pairs[modulo_page * 4:4 * (modulo_page + 1)] + [
+    if len(pairs) > 8:
+        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⇜", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("✗", callback_data="aries_back"),
              EqInlineKeyboardButton("⇝", callback_data="{}_next({})".format(prefix, modulo_page)))]
