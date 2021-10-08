716b7064bfaa31ac53e74b418302c4e45a7bae12
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-08 11:05:46 +0700

    Update rmbg.py

--- aries/modules/rmbg.py
+++ aries/modules/rmbg.py
@@ -26,7 +26,7 @@ from telethon import types
 from telethon.tl import functions
 
 from aries.events import register
-from aries import tbot
+from telethon import client as tbot
 
 REM_BG_API_KEY = "gUPv6BSmLzM861tdXZMyngVB"
 TEMP_DOWNLOAD_DIRECTORY = "./"
