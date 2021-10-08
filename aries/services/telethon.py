0e5eb9107d97f4db3ca5fa6ee1a570b9f59095a0
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-08 11:55:06 +0700

    Update telethon.py

--- aries/services/telethon.py
+++ aries/services/telethon.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
 
-from confing import get_int_key, get_str_key
+from aries.confing import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 NAME = TOKEN.split(":")[0]
