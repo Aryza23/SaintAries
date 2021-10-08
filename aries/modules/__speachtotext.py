8f65186b7b2374c946b6257dcb62b615c3524b5c
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-08 08:18:12 +0700

    Update __speachtotext.py

--- aries/modules/__speachtotext.py
+++ aries/modules/__speachtotext.py
@@ -14,6 +14,9 @@ from telethon.tl.types import *
 from aries import *
 from aries.events import register
 
+IBM_WATSON_CRED_URL = "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/a555b290-7e0f-4e20-a825-68ab2d471851"
+IBM_WATSON_CRED_PASSWORD = "rTnwgwsh3sYUVW162a8N6VCdv9MggyKL7FrtdXw97jU_"
+
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
