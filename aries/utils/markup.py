dbee62405a7e4ce9d5b54fba576484599330f704
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-09 21:01:59 +0700

    Create markup.py

---
+++ aries/utils/markup.py
@@ -0,0 +1,9 @@
+
+def MakeCaptchaMarkup(markup, _number, sign):
+    __markup = markup
+    for i in markup:
+        for k in i:
+            if k["text"] == _number:
+                k["text"] = f"{sign}"
+                k["callback_data"] = "done_"
+                return __markup
