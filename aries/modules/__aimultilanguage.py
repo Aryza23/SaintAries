9de78ef5ce128a11e1de4f38fcba79e6ac2f7505
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-09 13:17:38 +0700

    Update __aimultilanguage.py

--- aries/modules/__aimultilanguage.py
+++ aries/modules/__aimultilanguage.py
@@ -116,7 +116,7 @@ async def hmm(client, message):
         test = msg
         test = test.replace("hello", "Aco")
         test = test.replace("hello", "Aco")
-        URL = "http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}&botname=@idzeroid_bot&ownername=@IdzXartez"
+        URL = "http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}"
 
         try:
             r = requests.request("GET", url=URL)
@@ -179,7 +179,7 @@ async def hmm(client, message):
         # Kang with the credits bitches @InukaASiTH
         test = test.replace("hallo", "Aco")
         test = test.replace("hallo", "Aco")
-        URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}&botname=@idzeroid_bot&ownername=@IdzXartez"
+        URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}"
         try:
             r = requests.request("GET", url=URL)
         except:
@@ -253,7 +253,7 @@ async def inuka(client, message):
     # Kang with the credits bitches @InukaASiTH
     test = test.replace("hello", "Aco")
     test = test.replace("hello", "Aco")
-    URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}&botname=@idzeroid_bot&ownername=@IdzXartez"
+    URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}"
     try:
         r = requests.request("GET", url=URL)
     except:
@@ -331,7 +331,7 @@ async def inuka(client, message):
     # Kang with the credits bitches @InukaASiTH
     test = test.replace("hallo", "Aco")
     test = test.replace("hi", "Aco")
-    URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}&botname=@idzeroid_bot&ownername=@IdzXartez"
+    URL = f"http://kuki-api.tk/api/reply/kuki/moezilla/male/2/language=auto/message={test}"
     try:
         r = requests.request("GET", url=URL)
     except:
