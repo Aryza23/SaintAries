1e85844005c5a36adfe8a158618f671c4ff1e167
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-09 20:54:38 +0700

    Create captcha_mongo.py

---
+++ aries/modules/mongo/captcha_mongo.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from aries import Config
+
+class manage_db():
+    def __init__(self):
+        self.db = MongoClient(Config.MONGODB_URI)["captcha"]
+        self.chats = self.db["Chats"]
+        
+    def chat_in_db(self, chat_id):
+        chat = self.chats.find_one({"chat_id":chat_id})
+        return chat
+        
+    def add_chat(self, chat_id, captcha):
+        if self.chat_in_db(chat_id):
+            return 404
+        self.chats.insert_one({"chat_id":chat_id, "captcha": captcha})
+        return 200
+    
+    def delete_chat(self,chat_id):
+        self.chats.delete_many({"chat_id": chat_id})
+        return True
