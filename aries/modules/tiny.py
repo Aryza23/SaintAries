8dbfc773104a12fda1cfc47acb9a7865253898e7
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-14 23:31:50 +0700

    Create tiny.py

---
+++ aries/modules/tiny.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup
+import urllib
+from aries import telethn as tbot
+import glob
+import io
+import os
+import re
+import aiohttp
+import urllib.request
+from urllib.parse import urlencode
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from search_engine_parser import GoogleSearch
+
+import bs4
+import html2text
+from bing_image_downloader import downloader
+from telethon import *
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from aries import *
+from aries.events import register
+
+
+@register(pattern="^/tiny (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    
+async def ultiny(event):
+    reply = await event.get_reply_message()
+    if not (reply and (reply.media)):
+        await event.edit("`Reply Sticker !`")
+        return
+    xx = await event.edit("`Processing tiny...`")
+    ik = await bot.download_media(reply)
+    im1 = Image.open("resources/man_blank.png")
+    if ik.endswith(".tgs"):
+        await event.client.download_media(reply, "ult.tgs")
+        os.system("lottie_convert.py ult.tgs json.json")
+        with open("json.json", "r") as json:
+            jsn = json.read()
+        jsn = jsn.replace("512", "2000")
+        open("json.json", "w").write(jsn)
+        os.system("lottie_convert.py json.json ult.tgs")
+        file = "ult.tgs"
+        os.remove("json.json")
+    elif ik.endswith((".gif", ".mp4")):
+        iik = cv2.VideoCapture(ik)
+        dani, busy = iik.read()
+        cv2.imwrite("i.png", busy)
+        fil = "i.png"
+        im = Image.open(fil)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove(fil)
+        os.remove("k.png")
+    else:
+        im = Image.open(ik)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove("k.png")
+    await event.client.send_file(event.chat_id, file, reply_to=event.reply_to_msg_id)
+    await xx.delete()
+    os.remove(file)
+    os.remove(ik)
+
+
+__mod_name__ = "Tiny"
