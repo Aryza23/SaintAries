28c19a9aa9205df9a30d8e46c6713c4d8c11b296
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-14 23:55:53 +0700

    Update rmbg.py

--- aries/modules/rmbg.py
+++ aries/modules/rmbg.py
@@ -72,7 +72,7 @@ async def _(event):
         await event.reply("Background Removed in {} seconds".format(ms))
     else:
         await event.reply(
-            "remove.bg API returned Errors. Please report to @Black_Knights_Union_Support\n`{}".format(
+            "remove.bg API returned Errors. Please report to @idzeroidsupport\n`{}".format(
                 output_file_name.content.decode("UTF-8")
             )
         )
