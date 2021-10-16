77a2b29a70fbf3b108a7b734745837eccbae6b49
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-16 14:23:32 +0700

    Update extraction.py

--- aries/modules/helper_funcs/extraction.py
+++ aries/modules/helper_funcs/extraction.py
@@ -1,6 +1,3 @@
-se, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
 from typing import List, Optional
 
 from aries import LOGGER
