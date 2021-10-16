8e7f3590cedec30231a54565d83f25d5784901d7
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-16 11:11:34 +0700

    Update songdl.py

--- aries/modules/songdl.py
+++ aries/modules/songdl.py
@@ -47,13 +47,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("`Downloading Song... Please wait ⏱`")
+    m.edit("`Downloading Song... Please wait`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🔘 **Title**: [{title[:35]}]({link})\n🔘 **Source**: YouTube\n⏱🔘 **Duration**: `{duration}`\n🔘 **Views**: `{views}`\n🔥 **By**: @{BOT_USERNAME}'
+        rep = f'🔘 **Title**: [{title[:35]}]({link})\n🔘 **Source**: YouTube\n🔘 **Duration**: `{duration}`\n🔘 **Views**: `{views}`\n🔥 **By**: @{BOT_USERNAME}'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
