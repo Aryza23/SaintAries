fcf211a966f07648429baaa41c591fd26dd06a8f
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-16 12:40:11 +0700

    Create heroku.py

--- aries/modules/heroku.py
+++ aries/modules/heroku.py
@@ -3,25 +3,28 @@ import math
 import os
 import heroku3
 import requests
-from aries import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+
+from aries import telethn, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
 from aries.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 
 
-@register(pattern="^/(set|see|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
+@register(pattern=r"^/(set|see|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
 async def variable(var):
     if var.fwd_from:
         return
-    if var.sender_id == OWNER_ID:
-        pass
-    else:
+    if var.sender_id != OWNER_ID:
         return
+    """
+    Manage most of ConfigVars setting, set new var, get current var,
+    or delete var...
+    """
     if HEROKU_APP_NAME is not None:
         app = Heroku.app(HEROKU_APP_NAME)
     else:
-        return await var.reply("`[HEROKU]:" "\nPlease setup your` **HEROKU_APP_NAME**")
+        return await var.reply("`[HEROKU]:" f"\nPlease setup your` **{HEROKU_APP_NAME}**")
     exe = var.pattern_match.group(1)
     heroku_var = app.config()
     if exe == "see":
@@ -30,9 +33,12 @@ async def variable(var):
         try:
             variable = var.pattern_match.group(2).split()[0]
             if variable in heroku_var:
-                return await k.edit("**ConfigVars**:" f"\n\n`{variable} = {heroku_var[variable]}`\n")
-            else:
-                return await k.edit("**ConfigVars**:" f"\n\n`Error:\n-> {variable} don't exists`")
+                return await k.edit(
+                    "**ConfigVars**:" f"\n\n`{variable} = {heroku_var[variable]}`\n"
+                )
+            return await k.edit(
+                "**ConfigVars**:" f"\n\n`Error:\n-> {variable} don't exists`"
+            )
         except IndexError:
             configs = prettyjson(heroku_var.to_dict(), indent=2)
             with open("configs.json", "w") as fp:
@@ -69,9 +75,13 @@ async def variable(var):
                 return await s.edit(">`/set var <ConfigVars-name> <value>`")
         await asyncio.sleep(1.5)
         if variable in heroku_var:
-            await s.edit(f"**{variable}**  `successfully changed to`  ->  **{value}**")
+            await s.edit(
+                f"**{variable}**  `successfully changed to`  ->  **{value}**"
+            )
         else:
-            await s.edit(f"**{variable}**  `successfully added with value`  ->  **{value}**")
+            await s.edit(
+                f"**{variable}**  `successfully added with value`  ->  **{value}**"
+            )
         heroku_var[variable] = value
     elif exe == "del":
         m = await var.edit("`Getting information to deleting variable...`")
@@ -80,26 +90,28 @@ async def variable(var):
         except IndexError:
             return await m.edit("`Please specify ConfigVars you want to delete`")
         await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await m.edit(f"**{variable}**  `successfully deleted`")
-            del heroku_var[variable]
-        else:
+        if variable not in heroku_var:
             return await m.edit(f"**{variable}**  `is not exists`")
+
+        await m.edit(f"**{variable}**  `successfully deleted`")
+        del heroku_var[variable]
 
 
 @register(pattern="^/usage(?: |$)")
 async def dyno_usage(dyno):
     if dyno.fwd_from:
         return
-    if dyno.sender_id == OWNER_ID:
-        pass
-    else:
+    if dyno.sender_id != OWNER_ID:
         return
+    """
+    Get your account Dyno Usage
+    """
     die = await dyno.reply("**Processing...**")
     useragent = (
         "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/80.0.3987.149 Mobile Safari/537.36")
+        "Chrome/80.0.3987.149 Mobile Safari/537.36"
+    )
     user_id = Heroku.account().id
     headers = {
         "User-Agent": useragent,
@@ -109,7 +121,9 @@ async def dyno_usage(dyno):
     path = "/accounts/" + user_id + "/actions/get-quota"
     r = requests.get(heroku_api + path, headers=headers)
     if r.status_code != 200:
-        return await die.edit("`Error: something bad happened`\n\n" f">.`{r.reason}`\n")
+        return await die.edit(
+            "`Error: something bad happened`\n\n" f">.`{r.reason}`\n"
+        )
     result = r.json()
     quota = result["account_quota"]
     quota_used = result["quota_used"]
@@ -139,42 +153,13 @@ async def dyno_usage(dyno):
     return await die.edit(
         "**Dyno Usage**:\n\n"
         f" -> `Dyno usage for`  **{HEROKU_APP_NAME}**:\n"
-        f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
+        f"     🔘  `{AppHours}`**h**  `{AppMinutes}`**m**  "
         f"**|**  [`{AppPercentage}`**%**]"
         "\n\n"
         " -> `Dyno hours quota remaining this month`:\n"
-        f"     •  `{hours}`**h**  `{minutes}`**m**  "
+        f"     🔘  `{hours}`**h**  `{minutes}`**m**  "
         f"**|**  [`{percentage}`**%**]"
     )
-
-
-@register(pattern="^/log$")
-async def _(dyno):
-    if dyno.fwd_from:
-        return
-    if dyno.sender_id == OWNER_ID:
-        pass
-    else:
-        return
-    try:
-        Heroku = heroku3.from_key(HEROKU_API_KEY)
-        app = Heroku.app(HEROKU_APP_NAME)
-    except:
-        return await dyno.reply(" Please make sure your Heroku API Key, Your App name are configured correctly in the heroku")
-    v = await dyno.reply("Getting Logs....")
-    with open("logs.txt", "w") as log:
-        log.write(app.get_log())
-    await v.edit("Got the logs wait a sec")
-    await dyno.client.send_file(
-        dyno.chat_id,
-        "logs.txt",
-        reply_to=dyno.id,
-        caption="Aries Heroku Log.",
-    )
-
-    await asyncio.sleep(5)
-    await v.delete()
-    return os.remove("logs.txt")
 
 
 def prettyjson(obj, indent=2, maxlinelength=80):
@@ -189,3 +174,19 @@ def prettyjson(obj, indent=2, maxlinelength=80):
         indent=indent,
     )
     return indentitems(items, indent, level=0)
+
+def indentitems(items, indent, level):
+    """Recursively traverses the list of json lines, adds indentation based on the current depth"""
+    res = ""
+    indentstr = " " * (indent * level)
+    for (i, item) in enumerate(items):
+        if isinstance(item, list):
+            res += indentitems(item, indent, level + 1)
+        else:
+            islast = (i == len(items) - 1)
+            # no new line character after the last rendered line
+            if level == 0 and islast:
+                res += indentstr + item
+            else:
+                res += indentstr + item + "\n"
+    return res
