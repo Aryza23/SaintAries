adfe133916fa552cf67105493ea475679b0d9ef6
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-17 12:49:19 +0700

    Create kuki.js

---
+++ kuki.js
@@ -0,0 +1,20 @@
+const Slimbot = require('slimbot');
+const slimbot = new Slimbot(process.env.token);
+const Kukibot = require("kuki-bot-api");
+const kukiai  =  new  Kukibot({name: (process.env.name), owner: (process.env.owner)});
+
+
+
+slimbot.on('message', message => {
+var text = message.text
+  kukiai.moe(text)
+ .then(reply => {
+  slimbot.sendMessage(message.chat.id, reply);
+ })
+if (message.text.toLowerCase()=='/start'){
+        slimbot.sendMessage(message.chat.id, "I am kuki");
+    }
+});
+
+
+slimbot.startPolling();
