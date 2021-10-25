4d5ba8d971240fed6fbf94776b03cf05b9207417
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 04:06:47 +0700

    Update inlinefuncs.py

--- aries/utils/inlinefuncs.py
+++ aries/utils/inlinefuncs.py
@@ -28,7 +28,7 @@ from search_engine_parser import GoogleSearch
 
 from aries.extrastuff import SUDOERS
 from aries import EVENT_LOGS
-from aries import pbot
+from aries import pbot as app
 from aries.arqclient import arq
 from aries.utils.keyboard import ikb
 from aries.core.tasks import _get_tasks_text, all_tasks, rm_task
