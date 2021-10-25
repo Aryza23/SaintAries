34938c2c6b6ddeae343759393f5d5974231e1e97
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 01:38:56 +0700

    Update stickers.py

--- aries/modules/stickers.py
+++ aries/modules/stickers.py
@@ -398,7 +398,7 @@ def makepack_internal(
             success = context.bot.create_new_sticker_set(
                 user.id,
                 packname,
-                f"{name}s kang pack" + extra_version,
+                f"{name}s aries kang pack" + extra_version,
                 png_sticker=png_sticker,
                 emojis=emoji,
             )
