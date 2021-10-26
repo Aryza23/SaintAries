b0e749cbd78c0f372cc91f9358ab25ca3c09bba2
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 08:13:36 +0700

    Add files via upload

---
+++ aries/resources/imagefiles.py
@@ -0,0 +1,6 @@
+# This File Contains all the Picture Links used all over the Bot
+
+#Images in __main__.py
+EMILIA_IMG = "https://telegra.ph/file/dfec1a3dc126f2b5f779b.jpg"
+EMILIA_START_IMG = "https://telegra.ph/file/c657cde06a1dc14af5796.jpg"
+EMILIA_HELP_IMG = "https://telegra.ph/file/bf5b29a80aafa051ed894.jpg"
