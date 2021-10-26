41d2ab89da090512e210a84eca983f1dda574e41
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 12:05:39 +0700

    Update __init__.py

--- aries/utils/__init__.py
+++ aries/utils/__init__.py
@@ -1,2 +1,4 @@
 from .progress import progress
 from .tools import human_to_bytes, humanbytes, md5, time_formatter
+from .misc import paginate_modules  # noqa: F401
+from .read_lines import random_line  # noqa: F401
