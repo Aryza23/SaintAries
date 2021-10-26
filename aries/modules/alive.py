de66267343493dc5d594ff6c43af1d40c9cf96de
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 21:08:52 +0700

    Update alive.py

--- aries/modules/alive.py
+++ aries/modules/alive.py
@@ -2,9 +2,6 @@ import re, os
 from telethon import events, Button, custom
 from aries import IDZ, telethn as tbot
 from aries.events import register
-from sys import version as pyver
-from motor import version as mongover
-from pyrogram import __version__ as pyrover
 
 
 PHOTO = "https://telegra.ph/file/b4704fdf82e7a46cc5b51.jpg"
@@ -15,10 +12,10 @@ async def awake(event):
   aries = event.sender.first_name
   ARIES = "**Hello im Saint Aries** \n\n"
   ARIES += "**ALL SYSTEM WORKING PROPERLY**\n\n"
-  ARIES += " ☬ ⌊ **Python :** `{pyver.split()[0]}`⌉\n\n"
-  ARIES += " ☬ ⌊ **Pyrogram :** `{pyrover}`⌉\n\n"
-  ARIES += " ☬ ⌊ **MongoDB :** `{mongover}`⌉\n\n"
-  ARIES += " ☬ ⌊ **Platform :** `{sys.platform}` ⌉\n\n"
+  ARIES += " ☬ ⌊ **Python :** __3.9.7__ ⌉\n\n"
+  ARIES += " ☬ ⌊ **Pyrogram :** __1.2.9__ ⌉\n\n"
+  ARIES += " ☬ ⌊ **MongoDB :** __2.5.1__ ⌉\n\n"
+  ARIES += " ☬ ⌊ **Platform :** __linux__ ⌉\n\n"
   ARIES += " ☬ ⌊ **My Lord** : [Artezid](https://t.me/{IDZ}) ☠⌉\n\n"
   ARIES += " ☬ ⌊ **Sαιητ λrιεs** ⌉\n\n"
   ARIES += " ☬ ⌊ **TELETHON : 6.6.6 Latest** ⌉\n\n"
