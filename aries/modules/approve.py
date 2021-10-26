e61214e5603f2b5630a93f7a7ae9acc7399e8948
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-26 14:05:41 +0700

    Update approve.py

--- aries/modules/approve.py
+++ aries/modules/approve.py
@@ -13,7 +13,6 @@ from telegram.error import BadRequest
 
 @loggable
 @user_admin
-@run_async
 def approve(update, context):
     message = update.effective_message
     chat_title = message.chat.title
@@ -58,7 +57,6 @@ def approve(update, context):
 
 @loggable
 @user_admin
-@run_async
 def disapprove(update, context):
     message = update.effective_message
     chat_title = message.chat.title
@@ -96,7 +94,6 @@ def disapprove(update, context):
 
 
 @user_admin
-@run_async
 def approved(update, context):
     message = update.effective_message
     chat_title = message.chat.title
@@ -114,7 +111,6 @@ def approved(update, context):
 
 
 @user_admin
-@run_async
 def approval(update, context):
     message = update.effective_message
     chat = update.effective_chat
@@ -136,7 +132,6 @@ def approval(update, context):
         )
 
 
-@run_async
 def unapproveall(update: Update, context: CallbackContext):
     chat = update.effective_chat
     user = update.effective_user
@@ -167,7 +162,6 @@ def unapproveall(update: Update, context: CallbackContext):
         )
 
 
-@run_async
 def unapproveall_btn(update: Update, context: CallbackContext):
     query = update.callback_query
     chat = update.effective_chat
@@ -211,12 +205,12 @@ That's what approvals are for - approve of trustworthy users to allow them to se
 - `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
 """
 
-APPROVE = DisableAbleCommandHandler("approve", approve)
-DISAPPROVE = DisableAbleCommandHandler("unapprove", disapprove)
-APPROVED = DisableAbleCommandHandler("approved", approved)
-APPROVAL = DisableAbleCommandHandler("approval", approval)
-UNAPPROVEALL = DisableAbleCommandHandler("unapproveall", unapproveall)
-UNAPPROVEALL_BTN = CallbackQueryHandler(unapproveall_btn, pattern=r"unapproveall_.*")
+APPROVE = DisableAbleCommandHandler("approve", approve, run_async=True)
+DISAPPROVE = DisableAbleCommandHandler("unapprove", disapprove, run_async=True)
+APPROVED = DisableAbleCommandHandler("approved", approved, run_async=True)
+APPROVAL = DisableAbleCommandHandler("approval", approval, run_async=True)
+UNAPPROVEALL = DisableAbleCommandHandler("unapproveall", unapproveall, run_async=True)
+UNAPPROVEALL_BTN = CallbackQueryHandler(unapproveall_btn, pattern=r"unapproveall_.*", run_async=True)
 
 dispatcher.add_handler(APPROVE)
 dispatcher.add_handler(DISAPPROVE)
