2deaa24b544174dc69ddacf0ee1acfc78a505397
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-26 14:14:42 +0700

    Update connection.py

--- aries/modules/connection.py
+++ aries/modules/connection.py
@@ -14,7 +14,6 @@ user_admin = chat_status.user_admin
 
 
 @user_admin
-@run_async
 @typing_action
 def allow_connections(update, context) -> str:
 
@@ -62,7 +61,6 @@ def allow_connections(update, context) -> str:
         )
 
 
-@run_async
 @typing_action
 def connection_chat(update, context):
 
@@ -87,7 +85,6 @@ def connection_chat(update, context):
     send_message(update.effective_message, message, parse_mode="markdown")
 
 
-@run_async
 @typing_action
 def connect_chat(update, context):
 
@@ -318,7 +315,6 @@ CONN_HELP = """
  • More in future!"""
 
 
-@run_async
 def help_connect_chat(update, context):
 
     args = context.args
@@ -330,7 +326,6 @@ def help_connect_chat(update, context):
         send_message(update.effective_message, CONN_HELP, parse_mode="markdown")
 
 
-@run_async
 def connect_button(update, context):
 
     query = update.callback_query
@@ -402,14 +397,14 @@ This allows you to connect to a chat's database, and add things to it without th
  ❍ /allowconnect <yes/no>: allow a user to connect to a chat
 """
 
-CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, pass_args=True)
-CONNECTION_CHAT_HANDLER = CommandHandler("connection", connection_chat)
-DISCONNECT_CHAT_HANDLER = CommandHandler("disconnect", disconnect_chat)
+CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, pass_args=True, run_async=True)
+CONNECTION_CHAT_HANDLER = CommandHandler("connection", connection_chat, run_async=True)
+DISCONNECT_CHAT_HANDLER = CommandHandler("disconnect", disconnect_chat, run_async=True)
 ALLOW_CONNECTIONS_HANDLER = CommandHandler(
-    "allowconnect", allow_connections, pass_args=True
+    "allowconnect", allow_connections, pass_args=True, run_async=True
 )
-HELP_CONNECT_CHAT_HANDLER = CommandHandler("helpconnect", help_connect_chat)
-CONNECT_BTN_HANDLER = CallbackQueryHandler(connect_button, pattern=r"connect")
+HELP_CONNECT_CHAT_HANDLER = CommandHandler("helpconnect", help_connect_chat, run_async=True)
+CONNECT_BTN_HANDLER = CallbackQueryHandler(connect_button, pattern=r"connect", run_async=True)
 
 dispatcher.add_handler(CONNECT_CHAT_HANDLER)
 dispatcher.add_handler(CONNECTION_CHAT_HANDLER)
