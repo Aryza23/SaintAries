a4ceec75576c810acc0dd5814af3a13b89f69c2a
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-27 15:43:27 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -84,6 +84,7 @@ if ENV:
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1) # rest of connection code using the connection string `uri`
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     REDIS_URL = os.environ.get("REDIS_URL", None)
     IDZ = os.environ.get("IDZ", "IdzXartez")
@@ -112,10 +113,7 @@ if ENV:
     ARQ_API_URL = "https://thearq.tech"
     ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "ZBYMIN-TVRHON-OGTFXW-PUCAGK-ARQ")
     SAINT = "1192108540"
-
-    DATABASE_URL = os​.​getenv​(​"DATABASE_URL"​)  # or other relevant config varif uri.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1) # rest of connection code using the connection string `uri`
-
+    
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
     try:
