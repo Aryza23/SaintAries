1fe7435fde5954cfc831f1840359a107fbe98aba
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-27 12:28:53 +0700

    Create helpanime.py

---
+++ aries/modules/helpanime.py
@@ -0,0 +1,9 @@
+__mod_name__ = "🔘 Anime"
+__help__ = """
+Get information about anime, manga or characters with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
+*Available commands:*
+ - /manime <anime>: returns information about the anime.
+ - /mcharacter <character>: returns information about the character.
+ - /mmanga <manga>: returns information about the manga.
+ - /mupcoming: returns a list of new anime in the upcoming seasons.
+ """
