bb2fa6742ca295fbc5ff6c4212d9c05b8fbb94ff
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-27 12:56:09 +0700

    Update helpanime.py

--- aries/modules/helpanime.py
+++ aries/modules/helpanime.py
@@ -1,9 +1,21 @@
 __mod_name__ = "🔘 Anime"
 __help__ = """
-Get information about anime, manga or characters with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
+🔘 Get information about anime, manga or characters with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
 *Available commands:*
- ❍ /manime <anime>: returns information about the anime.
- ❍ /mcharacter <character>: returns information about the character.
- ❍ /mmanga <manga>: returns information about the manga.
- ❍ /mupcoming: returns a list of new anime in the upcoming seasons.
+ ❍ `/manime <anime>`*:* returns information about the anime.
+ ❍ `/mcharacter <character>`*:* returns information about the character.
+ ❍ `/mmanga <manga>`*:* returns information about the manga.
+ ❍ `/mupcoming`*:* returns a list of new anime in the upcoming seasons.
+
+🔘 Get information about anime, manga or characters from [AniList](anilist.co).
+*Available commands:*
+ ❍ `/anime <anime>`*:* returns information about the anime.
+ ❍ `/character <character>`*:* returns information about the character.
+ ❍ `/manga <manga>`*:* returns information about the manga.
+ ❍ `/user <user>`*:* returns information about a MyAnimeList user.
+ ❍ `/upcoming`*:* returns a list of new anime in the upcoming seasons.
+ ❍ `/kaizoku <anime>`*:* search an anime on animekaizoku.com
+ ❍ `/kayo <anime>`*:* search an anime on animekayo.com.
+ ❍ `/airing <anime>`*:* returns anime airing info.
+ ❍ `/whatanime`*:* reply to gif or video.
  """
