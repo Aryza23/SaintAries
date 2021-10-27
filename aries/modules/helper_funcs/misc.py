e126d4bc990c8dcf9c3f1be8a9abd462985c5942
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-27 11:32:13 +0700

    Create misc.py

--- aries/modules/helper_funcs/misc.py
+++ aries/modules/helper_funcs/misc.py
@@ -1,9 +1,8 @@
 from math import ceil
-from typing import Dict, List
 
-from aries import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
-from telegram.error import TelegramError
+from pyrogram.types import InlineKeyboardButton
+
+from aries import MOD_LOAD, MOD_NOLOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -17,116 +16,73 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
         return self.text > other.text
 
 
-def split_message(msg: str) -> List[str]:
-    if len(msg) < MAX_MESSAGE_LENGTH:
-        return [msg]
-
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
-
-    return result
-
-
-def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
+def paginate_modules(page_n, module_dict, prefix, chat=None):
     if not chat:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__MODULE__,
+                    callback_data="{}_module({})".format(
+                        prefix, x.__MODULE__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
     else:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__MODULE__,
+                    callback_data="{}_module({},{})".format(
+                        prefix, chat, x.__MODULE__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
 
-    pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
+    i = 0
+    for m in pairs:
+        for _ in m:
+            i += 1
+    if len(modules) - i == 1:
+        pairs.append((modules[-1],))
+    elif len(modules) - i == 2:
+        pairs.append(
+            (
+                modules[-2],
+                modules[-1],
+            )
+        )
 
-    round_num = len(modules) / 3
-    calc = len(modules) - round(round_num)
-    if calc == 1:
-        pairs.append((modules[-1], ))
-    elif calc == 2:
-        pairs.append((modules[-1], ))
-
-    max_num_pages = ceil(len(pairs) / 3)
+    max_num_pages = ceil(len(pairs) / 7)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 3:
-        pairs = pairs[modulo_page * 4:4 * (modulo_page + 4)] + [
-            (EqInlineKeyboardButton("⇜", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("✗", callback_data="aries_back"),
-             EqInlineKeyboardButton("⇝", callback_data="{}_next({})".format(prefix, modulo_page)))]
-
-    else:
-        pairs += [[EqInlineKeyboardButton("✗", callback_data="aries_back")]]
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 7 : 7 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "<",
+                    callback_data="{}_prev({})".format(
+                        prefix, modulo_page
+                    ),
+                ),
+                EqInlineKeyboardButton(
+                    ">",
+                    callback_data="{}_next({})".format(
+                        prefix, modulo_page
+                    ),
+                ),
+            )
+        ]
 
     return pairs
 
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
-    if html and markdown:
-        raise Exception("Can only send with either markdown or HTML!")
-    for user_id in set(send_to):
-        try:
-            if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
-            elif html:
-                bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
-            else:
-                bot.send_message(user_id, message)
-        except TelegramError:
-            pass  # ignore users who fail
-
-
-def build_keyboard(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
-def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
-
-
-def build_keyboard_parser(bot, chat_id, buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.url == "{rules}":
-            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
 def is_module_loaded(name):
-    return name not in NO_LOAD
+    return (
+        not MOD_LOAD or name in MOD_LOAD
+    ) and name not in MOD_NOLOAD
