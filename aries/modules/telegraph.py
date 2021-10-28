5a0a445722cf705a7d9e88b5f3c4e5a5ec1e4999
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-28 10:44:10 +0700

    Update telegraph.py

--- aries/modules/telegraph.py
+++ aries/modules/telegraph.py
@@ -92,8 +92,8 @@ file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 __help__ = """
- - /tm : Get Telegraph Link Of Replied Media
- - /tt: Get Telegraph Link of Replied Text
+ ❍ /tm : Get Telegraph Link Of Replied Media
+ ❍ /tt: Get Telegraph Link of Replied Text
 """
 
-__mod_name__ = "Telegraph"
+__mod_name__ = "🔘 Telegraph"
