b2ba7667b21c39f0e6db005fbf02ce4839bcecf6
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-29 03:09:22 +0700

    Update phone.py

--- aries/modules/phone.py
+++ aries/modules/phone.py
@@ -8,7 +8,7 @@ from aries.modules.helper_funcs.alternate import send_message
 from aries.modules.helper_funcs.chat_status import user_admin
 
 
-@run_async
+
 @user_admin
 def phone(update, context):
 
@@ -43,7 +43,7 @@ def phone(update, context):
     send_message(update.effective_message, g)
 
 
-PHONE_HANDLER = CommandHandler("phone", phone)
+PHONE_HANDLER = CommandHandler("phone", phone, run_async=True)
 
 dispatcher.add_handler(PHONE_HANDLER)
 
