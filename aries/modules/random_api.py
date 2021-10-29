58cb426d0426430fcdc6ddd71c2889240efe0c68
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-29 21:28:11 +0700

    Update random_api.py

--- aries/modules/random_api.py
+++ aries/modules/random_api.py
@@ -21,3 +21,12 @@ async def chika(event):
         return await tbot.send_file(event.chat_id, chikanya)
     except Exception:
         await event.reply("`Error 404 not found...`")
+
+@register(pattern="^bocil ?(.*)")
+async def bocil(event):
+    try:
+        resp = requests.get("https://feri-api.herokuapp.com/api/asupan/bocil").json()
+        bocilnya = f"{resp['url']}"
+        return await tbot.send_file(event.chat_id, bocilnya)
+    except Exception:
+        await event.reply("`Error 404 not found...`")
