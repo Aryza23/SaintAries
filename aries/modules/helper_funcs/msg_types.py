8aa27ee1ddc80347a1bb34669606dabade377874
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-29 17:05:38 +0700

    Update msg_types.py

--- aries/modules/helper_funcs/msg_types.py
+++ aries/modules/helper_funcs/msg_types.py
@@ -1,6 +1,7 @@
 from enum import IntEnum, unique
 
-from aries.modules.helper_funcs.string_handling import button_markdown_parser
+from aries.modules.helper_funcs.string_handling import \
+    button_markdown_parser
 from telegram import Message
 
 
@@ -21,27 +22,34 @@ def get_note_type(msg: Message):
     content = None
     text = ""
     raw_text = msg.text or msg.caption
-    args = raw_text.split(None, 2)  # use python's maxsplit to separate cmd and args
+    args = raw_text.split(None,
+                          2)  # use python's maxsplit to separate cmd and args
     note_name = args[1]
 
     buttons = []
     # determine what the contents of the filter are - text, image, sticker, etc
     if len(args) >= 3:
         offset = len(args[2]) - len(
-            raw_text,
-        )  # set correct offset relative to command + notename
+            raw_text)  # set correct offset relative to command + notename
         text, buttons = button_markdown_parser(
             args[2],
             entities=msg.parse_entities() or msg.parse_caption_entities(),
-            offset=offset,
-        )
-        data_type = Types.BUTTON_TEXT if buttons else Types.TEXT
+            offset=offset)
+        if buttons:
+            data_type = Types.BUTTON_TEXT
+        else:
+            data_type = Types.TEXT
+
     elif msg.reply_to_message:
         entities = msg.reply_to_message.parse_entities()
         msgtext = msg.reply_to_message.text or msg.reply_to_message.caption
         if len(args) >= 2 and msg.reply_to_message.text:  # not caption, text
             text, buttons = button_markdown_parser(msgtext, entities=entities)
-            data_type = Types.BUTTON_TEXT if buttons else Types.TEXT
+            if buttons:
+                data_type = Types.BUTTON_TEXT
+            else:
+                data_type = Types.TEXT
+
         elif msg.reply_to_message.sticker:
             content = msg.reply_to_message.sticker.file_id
             data_type = Types.STICKER
@@ -52,7 +60,8 @@ def get_note_type(msg: Message):
             data_type = Types.DOCUMENT
 
         elif msg.reply_to_message.photo:
-            content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
+            content = msg.reply_to_message.photo[
+                -1].file_id  # last elem = best quality
             text, buttons = button_markdown_parser(msgtext, entities=entities)
             data_type = Types.PHOTO
 
@@ -82,69 +91,73 @@ def get_welcome_type(msg: Message):
 
     try:
         if msg.reply_to_message:
-            args = msg.reply_to_message.text or msg.reply_to_message.caption
+            if msg.reply_to_message.text:
+                args = msg.reply_to_message.text
+            else:
+                args = msg.reply_to_message.caption
         else:
             args = msg.text.split(
-                None, 1,
-            )  # use python's maxsplit to separate cmd and args
+                None, 1)  # use python's maxsplit to separate cmd and args
     except AttributeError:
         args = False
 
-    if msg.reply_to_message:
-        if msg.reply_to_message.sticker:
-            content = msg.reply_to_message.sticker.file_id
-            text = None
-            data_type = Types.STICKER
+    if msg.reply_to_message and msg.reply_to_message.sticker:
+        content = msg.reply_to_message.sticker.file_id
+        text = None
+        data_type = Types.STICKER
 
-        elif msg.reply_to_message.document:
-            content = msg.reply_to_message.document.file_id
-            text = msg.reply_to_message.caption
-            data_type = Types.DOCUMENT
+    elif msg.reply_to_message and msg.reply_to_message.document:
+        content = msg.reply_to_message.document.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.DOCUMENT
 
-        elif msg.reply_to_message.photo:
-            content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
-            text = msg.reply_to_message.caption
-            data_type = Types.PHOTO
+    elif msg.reply_to_message and msg.reply_to_message.photo:
+        content = msg.reply_to_message.photo[
+            -1].file_id  # last elem = best quality
+        text = msg.reply_to_message.caption
+        data_type = Types.PHOTO
 
-        elif msg.reply_to_message.audio:
-            content = msg.reply_to_message.audio.file_id
-            text = msg.reply_to_message.caption
-            data_type = Types.AUDIO
+    elif msg.reply_to_message and msg.reply_to_message.audio:
+        content = msg.reply_to_message.audio.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.AUDIO
 
-        elif msg.reply_to_message.voice:
-            content = msg.reply_to_message.voice.file_id
-            text = msg.reply_to_message.caption
-            data_type = Types.VOICE
+    elif msg.reply_to_message and msg.reply_to_message.voice:
+        content = msg.reply_to_message.voice.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.VOICE
 
-        elif msg.reply_to_message.video:
-            content = msg.reply_to_message.video.file_id
-            text = msg.reply_to_message.caption
-            data_type = Types.VIDEO
+    elif msg.reply_to_message and msg.reply_to_message.video:
+        content = msg.reply_to_message.video.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.VIDEO
 
-        elif msg.reply_to_message.video_note:
-            content = msg.reply_to_message.video_note.file_id
-            text = None
-            data_type = Types.VIDEO_NOTE
+    elif msg.reply_to_message and msg.reply_to_message.video_note:
+        content = msg.reply_to_message.video_note.file_id
+        text = None
+        data_type = Types.VIDEO_NOTE
 
     buttons = []
     # determine what the contents of the filter are - text, image, sticker, etc
     if args:
         if msg.reply_to_message:
-            argumen = msg.reply_to_message.caption or ""
+            argumen = msg.reply_to_message.caption if msg.reply_to_message.caption else ""
             offset = 0  # offset is no need since target was in reply
             entities = msg.reply_to_message.parse_entities()
         else:
             argumen = args[1]
             offset = len(argumen) - len(
-                msg.text,
-            )  # set correct offset relative to command + notename
+                msg.text)  # set correct offset relative to command + notename
             entities = msg.parse_entities()
         text, buttons = button_markdown_parser(
-            argumen, entities=entities, offset=offset,
-        )
+            argumen, entities=entities, offset=offset)
 
-    if not data_type and text:
-        data_type = Types.BUTTON_TEXT if buttons else Types.TEXT
+    if not data_type:
+        if text and buttons:
+            data_type = Types.BUTTON_TEXT
+        elif text:
+            data_type = Types.TEXT
+
     return text, data_type, content, buttons
 
 
@@ -155,11 +168,8 @@ def get_filter_type(msg: Message):
         text = msg.text.split(None, 2)[2]
         data_type = Types.TEXT
 
-    elif (
-        msg.reply_to_message
-        and msg.reply_to_message.text
-        and len(msg.text.split()) >= 2
-    ):
+    elif (msg.reply_to_message and msg.reply_to_message.text and
+          len(msg.text.split()) >= 2):
         content = None
         text = msg.reply_to_message.text
         data_type = Types.TEXT
@@ -175,7 +185,8 @@ def get_filter_type(msg: Message):
         data_type = Types.DOCUMENT
 
     elif msg.reply_to_message and msg.reply_to_message.photo:
-        content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
+        content = msg.reply_to_message.photo[
+            -1].file_id  # last elem = best quality
         text = msg.reply_to_message.caption
         data_type = Types.PHOTO
 
