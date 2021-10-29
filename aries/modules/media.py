4a3b853863787e1940de850a1cb4443614fbb7b9
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-29 10:34:26 +0700

    Update media.py

--- aries/modules/media.py
+++ aries/modules/media.py
@@ -7,7 +7,7 @@ from io import BytesIO
 from urllib.parse import urlparse
 
 import ffmpeg
-import yt-dlp
+import yt_dlp
 from pyrogram import filters
 
 from aries.arqclient import aiohttpsession as session
