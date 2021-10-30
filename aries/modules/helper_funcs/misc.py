c27751de31719f48c0b97b39eee58296a487c483
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-30 11:28:23 +0700

    Update misc.py

--- aries/modules/helper_funcs/misc.py
+++ aries/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7 : 7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("[⇜]", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("[❌]", callback_data="aries_back"),
+                EqInlineKeyboardButton("[❌]", callback_data="aboutmanu_"),
              EqInlineKeyboardButton("[⇝]", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("[❌]", callback_data="aries_back")]]
+        pairs += [[EqInlineKeyboardButton("[❌]", callback_data="aboutmanu_")]]
 
     return pairs
 
