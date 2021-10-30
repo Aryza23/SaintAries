f324d704e15d8dd5e3e65a6abe7ea6823d8376a4
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-30 15:33:31 +0700

    Update backups.py

--- aries/modules/backups.py
+++ aries/modules/backups.py
@@ -365,15 +365,15 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "🔘 Backups"
 
 __help__ = """
-*Only for group owner:*
+🔘*Only for group owner:*
 
- • /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ ❍ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- • /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ ❍ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
