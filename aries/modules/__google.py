eafd2c07674902353f962cc5572f7d3f35c3b4a6
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-31 11:28:57 +0700

    Update __google.py

--- aries/modules/__google.py
+++ aries/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "GOOGLE"
+__mod_name__ = "🔘 Google"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
