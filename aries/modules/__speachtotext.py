85b01fdbcf5279fdfec01dfd90ddb0514a123308
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-01 21:21:13 +0700

    Update __speachtotext.py

--- aries/modules/__speachtotext.py
+++ aries/modules/__speachtotext.py
@@ -101,7 +101,7 @@ __help__ = """
 I can convert text to voice and voice to text..
  ❍ /tts <lang code>*:* Reply to any message to get text to speech output
  ❍ /stt*:* Type in reply to a voice message(support english only) to extract text from it.
-*Language Codes*
+🔘 *Language Codes*
 `af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
 et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
 id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
@@ -110,4 +110,4 @@ sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
 
-__mod_name__ = "TTS/STT"
+__mod_name__ = "🔘 TTS/STT"
