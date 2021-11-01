97b4934c0b4c27e0082c61093afe7d7c9b903c8a
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-01 19:44:52 +0700

    Update musicplayer.py

--- aries/modules/musicplayer.py
+++ aries/modules/musicplayer.py
@@ -1,44 +1,37 @@
-__mod_name__ = "Music Player"
+__mod_name__ = "🔘 Music Player"
 __help__ = """
-          <b>》** Saint Aries Music ** 《</b>
+          》** Saint Aries Music **《
             
- Saint Aries can plays music in your group's voice chat specialy made by @IdzXartez
- 
+ Saint Aries can plays music in your group's voice chat specialy made by @IdzXartez 
  Assistant name >> Ask At @Idzeroidsupport For assistant music 
  
-<b> Setting up </b>
+🔘 Setting up
+❍ Make bot admin
+❍ Invite Assistant with type /userbotjoin or ask at @idzeroidsupport for assistant
+❍ Start a voice chat
+❍ Try `/play [song name]` for the first time by an admin
+❍ If userbot joined enjoy music, If not add manually to your group and retry
 
-1) Make bot admin
-2) Invite Assistant with type /userbotjoin or ask at @idzeroidsupport for assistant
-2) Start a voice chat
-3) Try `/play [song name]` for the first time by an admin
-*) If userbot joined enjoy music, If not add manually to your group and retry
-
-<b> Commands </b>
-
-=>> Song Playing 🎧 
-
-  /play: Play song using youtube music
-  /play [yt url] : Play the given yt url
-  /video: download video song 
-  /song: download song via youtube
+🔘 Commands 
+⇝ Song Playing
+❍ /play: Play song using youtube music
+❍ /play [yt url] : Play the given yt url
+❍ /video: download video song 
+❍ /song: download song via youtube
   
-=>> Playback ⏯
+⇝ Playback
+❍ /player: Open Settings menu of player
+❍ /skip: Skips the current track
+❍ /pause: Pause track
+❍ /resume: Resumes the paused track
+❍ /end: Stops media playback
+❍ /current: Shows the current Playing track
+❍ /playlist: Shows playlist
 
-  /player: Open Settings menu of player
-  /skip: Skips the current track
-  /pause: Pause track
-  /resume: Resumes the paused track
-  /end: Stops media playback
-  /current: Shows the current Playing track
-  /playlist: Shows playlist
+⇝ More tools 
+❍ /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+❍ /userbotjoin: Fot Invite Assistan Music Userbot to your chat anf play the music
 
-=>> More tools 🧑‍🔧
-
-  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-  /userbotjoin: Fot Invite Assistan Music Userbot to your chat anf play the music
-
-*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
-
-<b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+🔘 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+🔘 PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME
 """
