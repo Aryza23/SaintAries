25fdebd20555a34800418faa8e74778744112bb6
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-02 15:30:39 +0700

    Update handlers.py

--- aries/modules/helper_funcs/handlers.py
+++ aries/modules/helper_funcs/handlers.py
@@ -11,7 +11,10 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 import aries.modules.sql.blacklistusers_sql as sql
 from aries import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
-CMD_STARTERS = ("/", "!") if ALLOW_EXCL else ("/",)
+if ALLOW_EXCL:
+    CMD_STARTERS = ("/", "!")
+else:
+    CMD_STARTERS = ("/",)
 
 
 class AntiSpam:
@@ -64,40 +67,41 @@ class CustomCommandHandler(CommandHandler):
             )
 
     def check_update(self, update):
-        if not isinstance(update, Update) or not update.effective_message:
-            return
-        message = update.effective_message
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
 
-        try:
-            user_id = update.effective_user.id
-        except:
-            user_id = None
+            try:
+                user_id = update.effective_user.id
+            except:
+                user_id = None
 
-        if user_id and sql.is_user_blacklisted(user_id):
-            return False
+            if user_id:
+                if sql.is_user_blacklisted(user_id):
+                    return False
 
-        if message.text and len(message.text) > 1:
-            fst_word = message.text.split(None, 1)[0]
-            if len(fst_word) > 1 and any(
-                fst_word.startswith(start) for start in CMD_STARTERS
-            ):
-
-                args = message.text.split()[1:]
-                command = fst_word[1:].split("@")
-                command.append(message.bot.username)
-                if user_id == 1087968824:
-                    user_id = update.effective_chat.id
-                if not (
-                    command[0].lower() in self.command
-                    and command[1].lower() == message.bot.username.lower()
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(
+                    fst_word.startswith(start) for start in CMD_STARTERS
                 ):
-                    return None
-                if SpamChecker.check_user(user_id):
-                    return None
-                filter_result = self.filters(update)
-                if filter_result:
-                    return args, filter_result
-                return False
+
+                    args = message.text.split()[1:]
+                    command = fst_word[1:].split("@")
+                    command.append(message.bot.username)
+                    if user_id == 1087968824:
+                        user_id = update.effective_chat.id
+                    if not (
+                        command[0].lower() in self.command
+                        and command[1].lower() == message.bot.username.lower()
+                    ):
+                        return None
+                    if SpamChecker.check_user(user_id):
+                        return None
+                    filter_result = self.filters(update)
+                    if filter_result:
+                        return args, filter_result
+                    else:
+                        return False
 
     def handle_update(self, update, dispatcher, check_result, context=None):
         if context:
