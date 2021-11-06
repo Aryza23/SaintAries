3e43abc32ed0f6c2e107b1a9e96cce2e05071206
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-06 18:28:39 +0700

    Update musicplayer.py

--- aries/modules/musicplayer.py
+++ aries/modules/musicplayer.py
@@ -1,34 +1,15 @@
 __help__ = """
-*<b>Saint Aries can plays music in your group's voice chat specialy made by @IdzXartez 
- Assistant name >> Ask At @Idzeroidsupport For assistant music</b>*
- 
-🔘 Setting up
-❍ Make bot admin
-❍ Invite Assistant with type /userbotjoin or ask at @idzeroidsupport for assistant
-❍ Start a voice chat
-❍ Try `/play [song name]` for the first time by an admin
-❍ If userbot joined enjoy music, If not add manually to your group and retry
+🔘 Setting up.
+❍ Make bot admin.
+❍ Invite Assistant with type /userbotjoin or ask at @idzeroidsupport for assistant.
+❍ Start a voice chat.
+❍ Try /play [song name] for the first time by an admin.
+❍ If userbot joined enjoy music, If not add manually to your group and retry.
 
-🔘 Commands 
-⇝ Song Playing
-❍ /play <i>query</i>: Play song using youtube music
-❍ /video <i>query</i>: download video song 
-❍ /song <i>query</i>: download song via youtube
-  
-⇝ Playback
-❍ /player: Open Settings menu of player
-❍ /skip: Skips the current track
-❍ /pause: Pause track
-❍ /resume: Resumes the paused track
-❍ /end: Stops media playback
-❍ /current: Shows the current Playing track
-❍ /playlist: Shows playlist
-
-⇝ More tools 
-❍ /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-
-🔘 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
-🔘 PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME
+🔘 Commands.
+⇝ Song Playing.
+❍ /play <i>query</i>: Play song using youtube music.
+❍ /video <i>query</i>: download video song.
+❍ /song <i>query</i>: download song via youtube.
 """
-
 __mod_name__ = "🔘 Music Player"
