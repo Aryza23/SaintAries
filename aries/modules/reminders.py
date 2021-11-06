f3fbed13a467de362a5e35251917ecd46ea6d500
aryazakaria01 <ardianrazak7107@gmail.com>
2021-11-06 11:38:42 +0000

    Auto Fixes: Code formatted

--- aries/modules/reminders.py
+++ aries/modules/reminders.py
@@ -2,14 +2,13 @@ import re
 import time
 
 from telegram import Update
-from telegram.ext import CommandHandler, run_async
+from telegram.ext import CommandHandler
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.filters import Filters
 from telegram.parsemode import ParseMode
 
-from aries import OWNER_ID, updater, dispatcher
+from aries import OWNER_ID, dispatcher, updater
 from aries.modules.disable import DisableAbleCommandHandler
-
 
 job_queue = updater.job_queue
 
@@ -18,12 +17,11 @@ def get_time(time: str) -> int:
     if time[-1] == "s":
         return int(time[:-1])
     if time[-1] == "m":
-        return int(time[:-1])*60
+        return int(time[:-1]) * 60
     if time[-1] == "h":
-        return int(time[:-1])*3600
+        return int(time[:-1]) * 3600
     if time[-1] == "d":
-        return int(time[:-1])*86400
-
+        return int(time[:-1]) * 86400
 
 
 reminder_message = """
@@ -31,6 +29,7 @@ Your reminder:
 {reason}
 <i>Which you timed {time} before in {title}</i>
 """
+
 
 def reminders(update: Update, context: CallbackContext):
     user = update.effective_user
@@ -42,17 +41,15 @@ def reminders(update: Update, context: CallbackContext):
             user_reminders.append(job.name[1:])
     if len(user_reminders) == 0:
         msg.reply_text(
-            text = "You don't have any reminders set or all the reminders you have set have been completed",
-            reply_to_message_id = msg.message_id
+            text="You don't have any reminders set or all the reminders you have set have been completed",
+            reply_to_message_id=msg.message_id,
         )
         return
     reply_text = "Your reminders (<i>Mentioned below are the <b>Timstamps</b> of the reminders you have set</i>):\n"
     for i, u in enumerate(user_reminders):
         reply_text += f"\n{i+1}. <code>{u}</code>"
     msg.reply_text(
-        text = reply_text,
-        reply_to_message_id = msg.message_id,
-        parse_mode = ParseMode.HTML
+        text=reply_text, reply_to_message_id=msg.message_id, parse_mode=ParseMode.HTML
     )
 
 
@@ -63,88 +60,84 @@ def set_reminder(update: Update, context: CallbackContext):
     reason = msg.text.split()
     if len(reason) == 1:
         msg.reply_text(
-            "No time and reminder to mention!",
-            reply_to_message_id = msg.message_id
+            "No time and reminder to mention!", reply_to_message_id=msg.message_id
         )
         return
     if len(reason) == 2:
         msg.reply_text(
-            "Nothing to reminder! Add a reminder",
-            reply_to_message_id = msg.message_id
+            "Nothing to reminder! Add a reminder", reply_to_message_id=msg.message_id
         )
         return
     t = reason[1].lower()
-    if not re.match(r'[0-9]+(d|h|m|s)', t):
+    if not re.match(r"[0-9]+(d|h|m|s)", t):
         msg.reply_text(
-            "Use a correct format of time!",
-            reply_to_message_id = msg.message_id
+            "Use a correct format of time!", reply_to_message_id=msg.message_id
         )
         return
+
     def job(context: CallbackContext):
         title = ""
-        if chat.type == "private": title += "this chat"
-        if chat.type != "private": title += chat.title
+        if chat.type == "private":
+            title += "this chat"
+        if chat.type != "private":
+            title += chat.title
         context.bot.send_message(
-            chat_id = user.id,
-            text = reminder_message.format(
-                reason = " ".join(reason[2:]),
-                time = t,
-                title = title
+            chat_id=user.id,
+            text=reminder_message.format(
+                reason=" ".join(reason[2:]), time=t, title=title
             ),
-            disable_notification = False,
-            parse_mode = ParseMode.HTML
+            disable_notification=False,
+            parse_mode=ParseMode.HTML,
         )
+
     job_time = time.time()
     job_queue.run_once(
-        callback = job, 
-        when = get_time(t), 
-        name = f"t{job_time}{user.id}".replace(".", "")
+        callback=job, when=get_time(t), name=f"t{job_time}{user.id}".replace(".", "")
     )
     msg.reply_text(
-        text = "Your reminder has been set after {time} from now!\nTimestamp: <code>{time_stamp}</code>".format(
-            time = t,
-            time_stamp = str(job_time).replace(".", "") + str(user.id)
-        ), 
-        reply_to_message_id = msg.message_id,
-        parse_mode = ParseMode.HTML
+        text="Your reminder has been set after {time} from now!\nTimestamp: <code>{time_stamp}</code>".format(
+            time=t, time_stamp=str(job_time).replace(".", "") + str(user.id)
+        ),
+        reply_to_message_id=msg.message_id,
+        parse_mode=ParseMode.HTML,
     )
-    
+
+
 def clear_reminder(update: Update, context: CallbackContext):
     user = update.effective_user
     msg = update.effective_message
     text = msg.text.split()
-    if len(text) == 1 or not re.match(r'[0-9]+', text[1]):
+    if len(text) == 1 or not re.match(r"[0-9]+", text[1]):
         msg.reply_text(
-            text = "No/Wrong timestamp mentioned",
-            reply_to_message_id = msg.message_id
+            text="No/Wrong timestamp mentioned", reply_to_message_id=msg.message_id
         )
         return
     if not text[1].endswith(str(user.id)):
         msg.reply_text(
-            text = "The timestamp mentioned is not your reminder!",
-            reply_to_message_id = msg.message_id
+            text="The timestamp mentioned is not your reminder!",
+            reply_to_message_id=msg.message_id,
         )
         return
     jobs = list(job_queue.get_jobs_by_name("t" + text[1]))
     if len(jobs) == 0:
         msg.reply_text(
-            text = "This reminder is already completed or either not set",
-            reply_to_message_id = msg.message_id
+            text="This reminder is already completed or either not set",
+            reply_to_message_id=msg.message_id,
         )
         return
     jobs[0].schedule_removal()
     msg.reply_text(
-        text = "Done cleared the reminder!",
-        reply_to_message_id = msg.message_id
+        text="Done cleared the reminder!", reply_to_message_id=msg.message_id
     )
+
 
 def clear_all_reminders(update: Update, context: CallbackContext):
     user = update.effective_user
     msg = update.effective_message
     if user.id != OWNER_ID:
         msg.reply_text(
-            text = "Who this guy not being the owner wants me clear all the reminders!!?",
-            reply_to_message_id = msg.message_id
+            text="Who this guy not being the owner wants me clear all the reminders!!?",
+            reply_to_message_id=msg.message_id,
         )
         return
     jobs = list(job_queue.jobs())
@@ -160,10 +153,9 @@ def clear_all_reminders(update: Update, context: CallbackContext):
         for i, u in enumerate(unremoved_reminders):
             reply_text += f"\n{i+1}. <code>{u}</code>"
     msg.reply_text(
-        text = reply_text,
-        reply_to_message_id = msg.message_id,
-        parse_mode = ParseMode.HTML
+        text=reply_text, reply_to_message_id=msg.message_id, parse_mode=ParseMode.HTML
     )
+
 
 def clear_all_my_reminders(update: Update, context: CallbackContext):
     user = update.effective_user
@@ -171,8 +163,7 @@ def clear_all_my_reminders(update: Update, context: CallbackContext):
     jobs = list(job_queue.jobs())
     if len(jobs) == 0:
         msg.reply_text(
-            text = "You don't have any reminders!",
-            reply_to_message_id = msg.message_id
+            text="You don't have any reminders!", reply_to_message_id=msg.message_id
         )
         return
     unremoved_reminders = []
@@ -188,10 +179,9 @@ def clear_all_my_reminders(update: Update, context: CallbackContext):
         for i, u in enumerate(unremoved_reminders):
             reply_text += f"\n{i+1}. <code>{u}</code>"
     msg.reply_text(
-        text = reply_text,
-        reply_to_message_id = msg.message_id,
-        parse_mode = ParseMode.HTML
+        text=reply_text, reply_to_message_id=msg.message_id, parse_mode=ParseMode.HTML
     )
+
 
 __mod_name__ = "🔘 Reminders"
 __help__ = """
@@ -212,13 +202,30 @@ __help__ = """
   ❍ `/clearreminder 1234567890123456789`
 """
 
-RemindersHandler = CommandHandler(['reminders', 'myreminders'], reminders, filters = Filters.chat_type.private, run_async=True)
-SetReminderHandler = DisableAbleCommandHandler('setreminder', set_reminder, run_async=True)
-ClearReminderHandler = DisableAbleCommandHandler('clearreminder', clear_reminder, run_async=True)
+RemindersHandler = CommandHandler(
+    ["reminders", "myreminders"],
+    reminders,
+    filters=Filters.chat_type.private,
+    run_async=True,
+)
+SetReminderHandler = DisableAbleCommandHandler(
+    "setreminder", set_reminder, run_async=True
+)
+ClearReminderHandler = DisableAbleCommandHandler(
+    "clearreminder", clear_reminder, run_async=True
+)
 ClearAllRemindersHandler = CommandHandler(
-    'clearallreminders', clear_all_reminders, filters = Filters.chat(OWNER_ID), run_async=True)
+    "clearallreminders",
+    clear_all_reminders,
+    filters=Filters.chat(OWNER_ID),
+    run_async=True,
+)
 ClearALLMyRemindersHandler = CommandHandler(
-    ['clearmyreminders', 'clearallmyreminders'], clear_all_my_reminders, filters = Filters.chat_type.private, run_async=True)
+    ["clearmyreminders", "clearallmyreminders"],
+    clear_all_my_reminders,
+    filters=Filters.chat_type.private,
+    run_async=True,
+)
 
 dispatcher.add_handler(RemindersHandler)
 dispatcher.add_handler(SetReminderHandler)
