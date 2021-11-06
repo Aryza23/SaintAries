7aaae79e16853bee644e157ea374371c97f312c7
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-06 23:23:21 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -27,9 +27,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-MOD_LOAD = []
-MOD_NOLOAD = []
-
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
@@ -99,11 +96,11 @@ if ENV:
     OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", True))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", True)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     WALL_API = os.environ.get("WALL_API", None)
