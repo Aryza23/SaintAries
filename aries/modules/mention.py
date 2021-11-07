cdd90ce05479318250c18f2d43c519a6912317ba
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-07 13:56:07 +0700

    Update mention.py

--- aries/modules/mention.py
+++ aries/modules/mention.py
@@ -6,9 +6,9 @@ import logging
 import os
 
 from telegraph import upload_file
-from telethon import events
+from telethon import events Client
 from telethon.tl.types import ChannelParticipantsAdmins
-
+from aries import telethn as tbot
 from aries import telethn as bot
 
 logging.basicConfig(
@@ -16,76 +16,8 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-moment_worker = []
 
-# tag
-@bot.on(events.NewMessage(pattern="^/tagall|/call|/tall|/all|#all|@all?(.*)"))
-async def mentionall(event):
-    global moment_worker
-    if event.is_private:
-        return await event.respond("Use This In Channel or Group!")
-
-    admins = []
-    async for admin in client.iter_participants(
-        event.chat_id, filter=ChannelParticipantsAdmins
-    ):
-        admins.append(admin.id)
-    if not event.sender_id in admins:
-        return await event.respond("Only Admin can use it.")
-
-    if event.pattern_match.group(1):
-        mode = "text_on_cmd"
-        msg = event.pattern_match.group(1)
-    elif event.reply_to_msg_id:
-        mode = "text_on_reply"
-        msg = event.reply_to_msg_id
-        if msg == None:
-            return await event.respond("I can't Mention Members for Old Post!")
-    elif event.pattern_match.group(1) and event.reply_to_msg_id:
-        return await event.respond(
-            "Give me can an Argument. Ex: `/tag Hey, Where are you`"
-        )
-    else:
-        return await event.respond("Reply to Message or Give Some Text To Mention!")
-
-    if mode == "text_on_cmd":
-        moment_worker.append(event.chat_id)
-        usrnum = 0
-        usrtxt = ""
-        async for usr in client.iter_participants(event.chat_id):
-            usrnum += 1
-            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-            if event.chat_id not in moment_worker:
-                await event.respond("Stopped!")
-                return
-            if usrnum == 5:
-                await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-                await asyncio.sleep(2)
-                usrnum = 0
-                usrtxt = ""
-
-    if mode == "text_on_reply":
-        moment_worker.append(event.chat_id)
-
-        usrnum = 0
-        usrtxt = ""
-        async for usr in client.iter_participants(event.chat_id):
-            usrnum += 1
-            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-            if event.chat_id not in moment_worker:
-                await event.respond("Stopped")
-                return
-            if usrnum == 5:
-                await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-                await asyncio.sleep(2)
-                usrnum = 0
-                usrtxt = ""
-
-
-# Cancle
-
-
-@bot.on(events.NewMessage(pattern="^/cancel$"))
+@tbot.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
     if not event.chat_id in spam_chats:
         return await event.respond("__There is no proccess on going...__")
@@ -98,7 +30,7 @@ async def cancel_spam(event):
 
 
 # telegraph
-@bot.on(events.NewMessage(pattern="^/t$"))
+@tbot.on(events.NewMessage(pattern="^/t$"))
 async def telegraph(client, message):
     replied = message.reply_to_message
     if not replied:
@@ -139,7 +71,7 @@ async def telegraph(client, message):
         os.remove(download_location)
 
 
-@bot.on(events.NewMessage(pattern="^/call ?(.*)"))
+@tbot.on(events.NewMessage(pattern="^/tagall|/call|/tall|/all|#all|@all ?(.*)"))
 async def mentionall(event):
     if event.is_private:
         return await event.respond(
@@ -196,7 +128,7 @@ async def mentionall(event):
                 usrtxt = ""
 
 
-@bot.on(events.NewMessage(pattern="/administrator"))
+@tbot.on(events.NewMessage(pattern="/administrator"))
 async def _(event):
     if event.fwd_from:
         return
