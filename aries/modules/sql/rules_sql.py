929eb721a5053f20cfb327d0ce6259a4fbd0bce5
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-07 07:12:29 +0700

    Update rules_sql.py

--- aries/modules/sql/rules_sql.py
+++ aries/modules/sql/rules_sql.py
@@ -1,8 +1,7 @@
 import threading
 
-from sqlalchemy import Column, String, UnicodeText, distinct, func
-
 from aries.modules.sql import BASE, SESSION
+from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
 class Rules(BASE):
