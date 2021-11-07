c16c68e1f5a912f3bf4956a2897aff59347b18e7
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-07 07:03:30 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -100,7 +100,7 @@ if ENV:
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", True)
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     WALL_API = os.environ.get("WALL_API", None)
