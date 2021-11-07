0230efd73e3879c31613eef33feb5f4313722459
idzero23 <idzero23@users.noreply.github.com>
2021-11-07 06:27:37 +0000

    Automated code formatting
    Signed-off-by: idzero23 <80420642+idzero23@users.noreply.github.com>

--- aries/modules/mention.py
+++ aries/modules/mention.py
@@ -15,89 +15,92 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s'
+    level=logging.INFO, format="%(name)s - [%(levelname)s] - %(message)s"
 )
 LOGGER = logging.getLogger(__name__)
 
 moment_worker = []
 
-#tag
+# tag
 @bot.on(events.NewMessage(pattern="^/tagall|/call|/tall|/all|#all|@all?(.*)"))
 async def mentionall(event):
-  global moment_worker
-  if event.is_private:
-    return await event.respond("Use This In Channel or Group!")
-  
-  admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
-    return await event.respond("Only Admin can use it.")
-  
-  if event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
-    mode = "text_on_reply"
-    msg = event.reply_to_msg_id
-    if msg == None:
-        return await event.respond("I can't Mention Members for Old Post!")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("Give me can an Argument. Ex: `/tag Hey, Where are you`")
-  else:
-    return await event.respond("Reply to Message or Give Some Text To Mention!")
-    
-  if mode == "text_on_cmd":
-    moment_worker.append(event.chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in moment_worker:
-        await event.respond("Stopped!")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-        await asyncio.sleep(2)
+    global moment_worker
+    if event.is_private:
+        return await event.respond("Use This In Channel or Group!")
+
+    admins = []
+    async for admin in client.iter_participants(
+        event.chat_id, filter=ChannelParticipantsAdmins
+    ):
+        admins.append(admin.id)
+    if not event.sender_id in admins:
+        return await event.respond("Only Admin can use it.")
+
+    if event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.reply_to_msg_id:
+        mode = "text_on_reply"
+        msg = event.reply_to_msg_id
+        if msg == None:
+            return await event.respond("I can't Mention Members for Old Post!")
+    elif event.pattern_match.group(1) and event.reply_to_msg_id:
+        return await event.respond(
+            "Give me can an Argument. Ex: `/tag Hey, Where are you`"
+        )
+    else:
+        return await event.respond("Reply to Message or Give Some Text To Mention!")
+
+    if mode == "text_on_cmd":
+        moment_worker.append(event.chat_id)
         usrnum = 0
         usrtxt = ""
-        
-  
-  if mode == "text_on_reply":
-    moment_worker.append(event.chat_id)
- 
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in moment_worker:
-        await event.respond("Stopped")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2)
+        async for usr in client.iter_participants(event.chat_id):
+            usrnum += 1
+            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+            if event.chat_id not in moment_worker:
+                await event.respond("Stopped!")
+                return
+            if usrnum == 5:
+                await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+                await asyncio.sleep(2)
+                usrnum = 0
+                usrtxt = ""
+
+    if mode == "text_on_reply":
+        moment_worker.append(event.chat_id)
+
         usrnum = 0
         usrtxt = ""
+        async for usr in client.iter_participants(event.chat_id):
+            usrnum += 1
+            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+            if event.chat_id not in moment_worker:
+                await event.respond("Stopped")
+                return
+            if usrnum == 5:
+                await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+                await asyncio.sleep(2)
+                usrnum = 0
+                usrtxt = ""
 
 
-# Cancle 
+# Cancle
+
 
 @bot.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
-  if not event.chat_id in spam_chats:
-    return await event.respond('__There is no proccess on going...__')
-  else:
-    try:
-      spam_chats.remove(event.chat_id)
-    except:
-      pass
-    return await event.respond('🖲 Stoped')
+    if not event.chat_id in spam_chats:
+        return await event.respond("__There is no proccess on going...__")
+    else:
+        try:
+            spam_chats.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond("🖲 Stoped")
 
 
-#telegraph 
+# telegraph
 @bot.on(events.NewMessage(pattern="^/t$"))
 async def telegraph(client, message):
     replied = message.reply_to_message
