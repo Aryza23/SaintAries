f24f0e43fda55f3aa65f49bb65459d5d2db211b0
idzero23 <idzero23@users.noreply.github.com>
2021-11-08 13:26:47 +0000

    Automated code formatting
    Signed-off-by: idzero23 <80420642+idzero23@users.noreply.github.com>

--- aries/modules/sql/__init__.py
+++ aries/modules/sql/__init__.py
@@ -4,12 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os, sys
 from aries import DB_URI, SAINT, telethn
+
 if SAINT == 1192108540:
-   print ("IDZ ADDED ")
+    print("IDZ ADDED ")
 else:
-   print ("YOU REMOVED IDZ NOW SEE")
-   os.execl(sys.executable, sys.executable, *sys.argv)
-   telethn.disconnect()
+    print("YOU REMOVED IDZ NOW SEE")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    telethn.disconnect()
+
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
