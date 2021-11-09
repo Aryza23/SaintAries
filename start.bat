07dcc078ed2fa75ea258d2cd28e67b8ee60fbf84
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-09 23:00:29 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saint Aries
-:: Enables virtual env mode and then starts aries
+TITLE aries
+:: Enables virtual env mode and then starts saitama
 env\scripts\activate.bat && py -m aries
