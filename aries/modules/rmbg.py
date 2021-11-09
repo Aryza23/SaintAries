c1f420de5843e1d82cbe10022e099a01412f5f7a
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-09 22:32:15 +0700

    Update rmbg.py

--- aries/modules/rmbg.py
+++ aries/modules/rmbg.py
@@ -59,7 +59,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "idz.png"
+            remove_bg_image.name = "idz.webp"
             await telethn.send_file(
                 event.chat_id,
                 remove_bg_image,
