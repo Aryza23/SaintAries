bbedc4d3b8643b4753d78f157b3016457dd46e1c
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-09 22:47:13 +0700

    Update events.py

--- aries/events.py
+++ aries/events.py
@@ -1,15 +1,20 @@
-import glob
+from telethon import events
+
+from aries import telethn
+import inspect
+import time
 import logging
-import sys
+import re
 from pathlib import Path
 
 from telethon import events
 
-from aries import telethn
-
+from aries import telethn as tbot
+import glob
+import sys
 
 def register(**args):
-    """Registers a new message."""
+    """ Registers a new message. """
     pattern = args.get("pattern", None)
 
     r_pattern = r"^[/!]"
@@ -27,7 +32,7 @@ def register(**args):
 
 
 def chataction(**args):
-    """Registers chat actions."""
+    """ Registers chat actions. """
 
     def decorator(func):
         telethn.add_event_handler(func, events.ChatAction(**args))
@@ -37,7 +42,7 @@ def chataction(**args):
 
 
 def userupdate(**args):
-    """Registers user updates."""
+    """ Registers user updates. """
 
     def decorator(func):
         telethn.add_event_handler(func, events.UserUpdate(**args))
@@ -47,7 +52,7 @@ def userupdate(**args):
 
 
 def inlinequery(**args):
-    """Registers inline query."""
+    """ Registers inline query. """
     pattern = args.get("pattern", None)
 
     if pattern is not None and not pattern.startswith("(?i)"):
@@ -61,7 +66,7 @@ def inlinequery(**args):
 
 
 def callbackquery(**args):
-    """Registers inline query."""
+    """ Registers inline query. """
 
     def decorator(func):
         telethn.add_event_handler(func, events.CallbackQuery(**args))
@@ -69,44 +74,11 @@ def callbackquery(**args):
 
     return decorator
 
-
-def aries(**args):
-    pattern = args.get("pattern", None)
-    args.get("disable_edited", False)
-    ignore_unsafe = args.get("ignore_unsafe", False)
-    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
-    args.get("group_only", False)
-    args.get("disable_errors", False)
-    args.get("insecure", False)
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    if "disable_edited" in args:
-        del args["disable_edited"]
-
-    if "ignore_unsafe" in args:
-        del args["ignore_unsafe"]
-
-    if "group_only" in args:
-        del args["group_only"]
-
-    if "disable_errors" in args:
-        del args["disable_errors"]
-
-    if "insecure" in args:
-        del args["insecure"]
-
-    if pattern:
-        if not ignore_unsafe:
-            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
-
-
 def load_module(shortname):
     if shortname.startswith("__"):
         pass
     elif shortname.endswith("_"):
         import importlib
-
         import aries.events
 
         path = Path(f"aries/modules/{shortname}.py")
@@ -117,7 +89,6 @@ def load_module(shortname):
         print("Successfully imported " + shortname)
     else:
         import importlib
-
         import aries.events
 
         path = Path(f"aries/modules/{shortname}.py")
@@ -125,8 +96,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.aries = aries
-        mod.tbot = telethn
+        mod.tbot = tbot
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
         sys.modules["aries.modules." + shortname] = mod
