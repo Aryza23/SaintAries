e4bc58341854eef749bcc0fd21dec21383bba3a3
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-09 07:37:01 +0700

    Create about.py

---
+++ aries/modules/about.py
@@ -0,0 +1,10 @@
+__help__ = """
+** ✨ Aries - A Powerful Telegram Group Manager ✨ **
+Powerful Abilities
+• Group Voice Chat Music Play ❤️
+• Youtube Downloader 🎵
+• And More ⚡
+Developer [Aryza](https://t.me/IdzXartez)
+Support Group [Support](https://t.me/idzeroidsupport)
+"""
+__mod_name__ = "🔘 About"
