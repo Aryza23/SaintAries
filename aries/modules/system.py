92d221a0fce995fb06d128410477c104b65e04cb
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-11 05:26:20 +0700

    Add files via upload

---
+++ aries/modules/system.py
@@ -0,0 +1,38 @@
+"""
+XIT License 2021
+Copyright (c) 2021 Damantha126 
+"""
+import asyncio
+import os
+import subprocess
+import time
+
+import psutil
+import aries.modules.sql.users_sql as sql
+from pyrogram import filters
+
+from aries import (bot_start_time, DEV_USERS, pbot)
+from aries.utils import formatter
+
+# Stats Module
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - bot_start_time)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    stats = f"""
+root@IdzXartez:~$ Aries:
+------------------
+├-☉️⇝  UPTIME: {formatter.get_readable_time((bot_uptime))}
+├-☉️⇝  BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+├-☉️⇝  CPU: {cpu}%
+├-☉️⇝ 🖲RAM: {mem}%
+├-☉️⇝  DISK: {disk}%
+├-☉️⇝  USERS: {sql.num_users()} users.
+└-☉️⇝  GROUPS: {sql.num_chats()} groups.
+"""
+
+    return stats
