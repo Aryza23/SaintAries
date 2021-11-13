9f2adf2cda2357cb545d09c87e752adf832e5cc1
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-13 14:47:05 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -225,7 +225,6 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 DRAGONS.add(OWNER_ID)
-DEV_USERS.add(DRAGONS)
 DEV_USERS.add(OWNER_ID)
 DEV_USERS.add(1669508271)
 DEV_USERS.add(1817146787)
