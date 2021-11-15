7103b9ed027e793236a4cdbe4538c4d0e87b0280
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-15 08:30:01 +0700

    Update kuki_redis.py

--- aries/modules/redis/kuki_redis.py
+++ aries/modules/redis/kuki_redis.py
@@ -1,30 +1,30 @@
 import ast as idz
-from aries import REDIS_URI
+from aries import REDIS_URL
 
 try:
-    idz.literal_eval(REDIS_URI.get("KUKIBOT"))
+    idz.literal_eval(REDIS_URL.get("KUKIBOT"))
 except BaseException:
-    REDIS_URI.set("KUKIBOT", "[]")
+    REDIS_URL.set("KUKIBOT", "[]")
 
 def is_kuki(chat_id):
-    chat = idz.literal_eval(REDIS_URI.get("KUKIBOT"))	
+    chat = idz.literal_eval(REDIS_URL.get("KUKIBOT"))	
     chat = list(chat)
     if chat_id in chat:
     	return True
     return False
 	   
 def set_kuki(chat_id):
-    chat = idz.literal_eval(REDIS_URI.get("KUKIBOT"))
+    chat = idz.literal_eval(REDIS_URL.get("KUKIBOT"))
     chat = list(chat)
     if chat_id not in chat:
     	chat.append(chat_id)
-    	REDIS_URI.set("KUKIBOT", str(chat))
+    	REDIS_URL.set("KUKIBOT", str(chat))
     return 
 	
 def rm_kuki(chat_id):
-	chat = idz.literal_eval(REDIS_URI.get("KUKIBOT"))
+	chat = idz.literal_eval(REDIS_URL.get("KUKIBOT"))
 	chat = list(chat)
 	if chat_id in chat:
 		chat.remove(chat_id)
-		REDIS_URI.set("KUKIBOT", str(chat))
+		REDIS_URL.set("KUKIBOT", str(chat))
 	return 
