3b8502169697411034a1587240dd2113454dbe8c
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-16 00:59:22 +0700

    Update alarm.py

--- aries/modules/alarm.py
+++ aries/modules/alarm.py
@@ -143,11 +143,11 @@ async def tikclock(event):
 
 __help__ = """
 Do your works at time, don't late.
-*Commands*
- • `/setalarm <(date) (time)|zone|reason>`*:* sets a alarm/reminder 
-*Syntax:* `/setalarm 01/01/2000 10:00:00 AM | Asia/Kolkata | breakfast`
+🔘 *Commands*
+❍ `/setalarm <(date) (time)|zone|reason>`*:* sets a alarm/reminder 
+ *Syntax:* `/setalarm 01/01/2000 10:00:00 AM | Asia/Kolkata | breakfast`
 *NOTE:*
 Please turn on notifications(PM/Group Chat) otherwise you will not get notification for the alarm!
 """
 
-__mod_name__ = "Alarm"
+__mod_name__ = "🔘 Alarm"
