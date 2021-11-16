b0e2541a25d96b8e84e24e81fe21a6fb1e4db557
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-16 22:20:08 +0700

    Fix the double client and the double sender

--- aries/modules/core.py
+++ aries/modules/core.py
@@ -1,122 +1,36 @@
-# credits @RoseLoverX
+from NaoRobot import telethn as tbot
+from NaoRobot.events import register
+import os
 import asyncio
 import os
-
-from aries import OWNER_ID
-from aries.events import register, load_module
-from pathlib import Path
-
-opn = []
-
-water = "./aries/resources/Aries.jpg"
-
+import time
+from datetime import datetime
+from NaoRobot import OWNER_ID, DEV_USERS
+from NaoRobot import TEMP_DOWNLOAD_DIRECTORY as path
+from NaoRobot import TEMP_DOWNLOAD_DIRECTORY
+from datetime import datetime
+water = './Natsunagi/resources/Aries.jpg'
+client = tbot
 
 @register(pattern=r"^/send ?(.*)")
 async def Prof(event):
-    if event.sender_id == OWNER_ID:
+    if event.sender_id == OWNER_ID or event.sender_id == DEV_USERS:
         pass
     else:
         return
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./aries/modules/{}.py".format(input_str)
+    the_plugin_file = "./Natsunagi/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
-        message_id = event.message.id
-        await event.client.send_file(
-            event.chat_id,
-            the_plugin_file,
-            force_document=True,
-            allow_cache=False,
-            thumb=thumb,
-            reply_to=message_id,
-        )
+     message_id = event.message.id
+     await event.client.send_file(
+             event.chat_id,
+             the_plugin_file,
+             force_document=True,
+             allow_cache=False,
+             thumb=thumb,
+             reply_to=message_id,
+         )
     else:
         await event.reply("No File Found!")
-
-
-@register(pattern="^/install")
-async def install(event):
-    if event.fwd_from:
-        return
-    if event.sender_id == OWNER_ID:
-        pass
-    else:
-        return
-    if event.reply_to_msg_id:
-        try:
-            downloaded_file_name = (
-                await event.client.download_media(  # pylint:disable=E0602
-                    await event.get_reply_message(),
-                    "aries/modules/",  # pylint:disable=E0602
-                )
-            )
-            if "(" not in downloaded_file_name:
-                path1 = Path(downloaded_file_name)
-                shortname = path1.stem
-                load_module(shortname.replace(".py", ""))
-                await event.reply(
-                    "Installed.... 👍\n `{}`".format(
-                        os.path.basename(downloaded_file_name)
-                    ),
-                )
-            else:
-                os.remove(downloaded_file_name)
-                k = await event.reply(
-                    "**Error!**\n⚠️Cannot Install! \n📂 File not supported \n Or Pre Installed Maybe..😁",
-                )
-                await asyncio.sleep(2)
-                await k.delete()
-        except Exception as e:  # pylint:disable=C0103,W0703
-            j = await event.reply(str(e))
-            await asyncio.sleep(3)
-            await j.delete()
-            os.remove(downloaded_file_name)
-    await asyncio.sleep(3)
-    await event.delete()
-
-
-@register(pattern="/open")
-async def _(event):
-    xx = await event.reply("Processing...")
-    if event.reply_to_msg_id:
-        a = await event.get_reply_message()
-        if a.media:
-            b = await a.download_media()
-            c = open(b, "r")
-            d = c.read()
-            c.close()
-            n = 4096
-            for bkl in range(0, len(d), n):
-                opn.append(d[bkl : bkl + n])
-            for bc in opn:
-                await event.client.send_message(
-                    event.chat_id,
-                    f"{bc}",
-                    reply_to=event.reply_to_msg_id,
-                )
-            await event.delete()
-            opn.clear()
-            os.remove(b)
-            await xx.delete()
-        else:
-            return await event.reply("Reply to a readable file")
-    else:
-        return await event.reply("Reply to a readable file")
-
-
-@register(pattern="^/make ?(.*)")
-async def get(event):
-    name = event.text[5:]
-    if name is None:
-        await event.reply("reply to text message as `.ttf <file name>`")
-        return
-    m = await event.get_reply_message()
-    if m.text:
-        with open(name, "w") as f:
-            f.write(m.message)
-        await event.delete()
-        await event.client.send_file(event.chat_id, name, force_document=True)
-        os.remove(name)
-    else:
-        await event.reply("reply to text message as `.ttf <file name>`")
