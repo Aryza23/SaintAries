5fff988660ab39862bedebf5d9dbf13ca3f6371e
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-16 19:27:50 +0700

    Update random_api.py

--- aries/modules/random_api.py
+++ aries/modules/random_api.py
@@ -1,27 +1,7 @@
 import requests
-import random
+
 from aries import telethn as tbot
 from aries.events import register
-
-APAKAH_STRINGS = (
-    "iya",
-    "tidak",
-    "mungkin",
-    "bisa jadi",
-    "anda benar sekali",
-    "anda salah besar",
-    "👍",
-    "👎",
-)
-
-
-@register(pattern="^/apakah ?(.*)")
-async def apakah(event):
-    kontol = event.pattern_match.group(1)
-    if not kontol:
-        await event.reply("Example: /apakah (text)")
-        return
-    await event.reply(random.choice(APAKAH_STRINGS))
 
 
 @register(pattern="^/asupan ?(.*)")
