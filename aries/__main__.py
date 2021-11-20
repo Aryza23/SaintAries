2161a39c013872b163f2ffeb109182a67e535791
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-20 22:12:15 +0700

    Update __main__.py

--- aries/__main__.py
+++ aries/__main__.py
@@ -223,8 +223,6 @@ def start(update: Update, context: CallbackContext):
                         ]
                     ),
                 )
-            elif args[0].lower() == "disasters":
-                IMPORTED["disasters"].send_disasters(update)
             elif args[0].lower().startswith("stngs_"):
                 match = re.match("stngs_(.*)", args[0].lower())
                 chat = dispatcher.bot.getChat(match.group(1))
