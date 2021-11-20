b356d838d4ca3a89724169e0b3c7be85b08793ef
idzero23 <idzero23@users.noreply.github.com>
2021-11-20 04:49:58 +0000

    Automated code formatting
    Signed-off-by: idzero23 <88209926+idzero23@users.noreply.github.com>

--- aries/function/carbonfunc.py
+++ aries/function/carbonfunc.py
@@ -6,6 +6,6 @@ from aries import aiohttpsession as aiosession
 async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiosession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())       
+        image = BytesIO(await resp.read())
     image.name = "Aries_carbon.png"
     return image
