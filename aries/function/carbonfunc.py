79d1a83675a85a288e7d9253c5d29faa4a8e6530
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-20 10:41:41 +0700

    Update carbonfunc.py

--- aries/function/carbonfunc.py
+++ aries/function/carbonfunc.py
@@ -7,5 +7,5 @@ async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiosession.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
-    image.name = "Aries_carbon.png"
+    image.name = "Aries_carbon.webp"
     return image
