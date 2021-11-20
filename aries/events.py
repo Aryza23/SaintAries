e4dfde340b7a02d4dcb4f8655436c6b75b1bd712
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-20 21:34:28 +0700

    Ok

--- aries/events.py
+++ aries/events.py
@@ -1,22 +1,11 @@
 from telethon import events
 
 from aries import telethn
-import inspect
-import time
-import logging
-import re
-from pathlib import Path
-
-from telethon import events
-
-from aries import telethn as tbot
-import glob
-import sys
 
 
 def register(**args):
     """Registers a new message."""
-    pattern = args.get("pattern", None)
+    pattern = args.get("pattern")
 
     r_pattern = r"^[/!]"
 
@@ -30,85 +19,3 @@ def register(**args):
         return func
 
     return decorator
-
-
-def chataction(**args):
-    """Registers chat actions."""
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.ChatAction(**args))
-        return func
-
-    return decorator
-
-
-def userupdate(**args):
-    """Registers user updates."""
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.UserUpdate(**args))
-        return func
-
-    return decorator
-
-
-def inlinequery(**args):
-    """Registers inline query."""
-    pattern = args.get("pattern", None)
-
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.InlineQuery(**args))
-        return func
-
-    return decorator
-
-
-def callbackquery(**args):
-    """Registers inline query."""
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.CallbackQuery(**args))
-        return func
-
-    return decorator
-
-
-def load_module(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        import importlib
-        import aries.events
-
-        path = Path(f"aries/modules/{shortname}.py")
-        name = "aries.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        print("Successfully imported " + shortname)
-    else:
-        import importlib
-        import aries.events
-
-        path = Path(f"aries/modules/{shortname}.py")
-        name = "aries.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.register = register
-        mod.tbot = tbot
-        mod.logger = logging.getLogger(shortname)
-        spec.loader.exec_module(mod)
-        sys.modules["aries.modules." + shortname] = mod
-        print("Successfully imported " + shortname)
-
-
-path = "aries/modules/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
