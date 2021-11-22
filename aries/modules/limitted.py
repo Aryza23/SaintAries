8126a921369c3bbc3cd5755ca2a135bd40a4e424
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-22 11:13:20 +0700

    Create limitted.py

---
+++ aries/modules/limitted.py
@@ -0,0 +1,33 @@
+
+# inspired from bin.py which was made by @danish_00
+# written by @senku_ishigamiii/@Uzumaki_Naruto_XD
+
+"""
+✘ Commands Available -
+
+• `{i}limited`
+   Check you are limited or not !
+"""
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from aries.events import register
+from aries import ubot
+
+
+@register(pattern="^limited ?(.*)")
+async def _(event):
+    chat = "@SpamBot"
+    msg = await event.reply("Checking If You Are Limited...")
+    async with ubot.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178220800)
+            )
+            await conv.send_message("/start")
+            response = await response
+            await ubot.send_read_acknowledge(chat)
+        except YouBlockedUserError:
+            await msg.edit("Boss! Please Unblock @SpamBot ")
+            return
+        await msg.edit(f"~ {response.message.message}")
