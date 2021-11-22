d2e471e3b95a0abbda2b7a867fb19eafc724899b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-22 16:29:42 +0700

    Update devian-art.py

--- aries/modules/devian-art.py
+++ aries/modules/devian-art.py
@@ -34,7 +34,7 @@ async def devian(event):
     out = []
     num = 0
     for on in res:
-        img = await event.download_file(on["src"], f"aries/utils/downloads/{match}-{num}.jpg")
+        img = await tbot.download_file(on["src"], f"aries/utils/downloads/{match}-{num}.jpg")
         num += 1
         out.append(img)
     if len(out) == 0:
