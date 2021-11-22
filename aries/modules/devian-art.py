8c9f77577f0002a6d909a8a1c38fe5f96b4a13eb
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-22 15:33:50 +0700

    Update devian-art.py

--- aries/modules/devian-art.py
+++ aries/modules/devian-art.py
@@ -23,10 +23,10 @@ from aries import pbot
 
 
 @register(pattern="^/devian ?(.*)")
-async def downakd(e):
-    match = e.pattern_match.group(1)
+async def devian(event):
+    match = event.pattern_match.group(1)
     if not match:
-        return await e.reply(e, "`Give Query to Search...`")
+        return await event.reply(event, "`Give Query to Search...`")
     Random = False
     if ";" in match:
         num = int(match.split(";")[1])
@@ -35,7 +35,7 @@ async def downakd(e):
         match = match.split(";")[0]
     else:
         num = 5
-    xd = await e.reply(e, "`Processing...`")
+    xd = await event.reply(event, "`Processing...`")
     match = match.replace(" ", "+")
     link = "https://www.deviantart.com/search?q=" + match
     ct = requests.get(link).content
@@ -53,7 +53,7 @@ async def downakd(e):
         out.append(img)
     if len(out) == 0:
         return await xd.edit("`No Results Found!`")
-    await e.client.send_file(
-        e.chat_id, out, caption=f"Uploaded {len(res)} Images\n", album=True
+    await event.client.send_file(
+        event.chat_id, out, caption=f"Uploaded {len(res)} Images\n", album=True
     )
     await xd.delete()
