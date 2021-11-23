fcc0d1957426dce0d3d0692a43be0730c5319289
idzero23 <idzero23@users.noreply.github.com>
2021-11-23 13:37:56 +0000

    Automated code formatting
    Signed-off-by: idzero23 <88209926+idzero23@users.noreply.github.com>

--- aries/modules/kamuii.py
+++ aries/modules/kamuii.py
@@ -5,7 +5,7 @@ from aries.events import register
 from aries import ubot, TEMP_DOWNLOAD_DIRECTORY
 
 
-@register(outgoing=True, pattern=r'^/kamuii(:? |$)([1-8])?')
+@register(outgoing=True, pattern=r"^/kamuii(:? |$)([1-8])?")
 async def _(fry):
     await fry.edit("`Prosess, Berubah menjadi srirahanjing, jurus di aktifkan...`")
     level = fry.pattern_match.group(2)
@@ -28,9 +28,7 @@ async def _(fry):
             msg = await conv.send_message(reply_message)
             if level:
                 m = f"/deepfry {level}"
-                msg_level = await conv.send_message(
-                    m,
-                    reply_to=msg.id)
+                msg_level = await conv.send_message(m, reply_to=msg.id)
                 r = await conv.get_response()
                 response = await conv.get_response()
             else:
@@ -44,30 +42,28 @@ async def _(fry):
             await fry.edit("`Mohon Matikan Setelan Forward Privasi...`")
         else:
             downloaded_file_name = await fry.client.download_media(
-                response.media,
-                TEMP_DOWNLOAD_DIRECTORY
+                response.media, TEMP_DOWNLOAD_DIRECTORY
             )
             await fry.client.send_file(
                 fry.chat_id,
                 downloaded_file_name,
                 force_document=False,
-                reply_to=message_id_to_reply
+                reply_to=message_id_to_reply,
             )
             """ - cleanup chat after completed - """
             try:
                 msg_level
             except NameError:
-                await fry.client.delete_messages(conv.chat_id,
-                                                 [msg.id, response.id])
+                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
             else:
                 await fry.client.delete_messages(
-                    conv.chat_id,
-                    [msg.id, response.id, r.id, msg_level.id])
+                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
+                )
     await fry.delete()
     return os.remove(downloaded_file_name)
 
 
-@register(outgoing=True, pattern=r'^.df(:? |$)([1-8])?')
+@register(outgoing=True, pattern=r"^.df(:? |$)([1-8])?")
 async def _(fry):
     await fry.edit("`Sedang Dalam Proses......`")
     level = fry.pattern_match.group(2)
@@ -90,9 +86,7 @@ async def _(fry):
             msg = await conv.send_message(reply_message)
             if level:
                 m = f"/deepfry {level}"
-                msg_level = await conv.send_message(
-                    m,
-                    reply_to=msg.id)
+                msg_level = await conv.send_message(m, reply_to=msg.id)
                 r = await conv.get_response()
                 response = await conv.get_response()
             else:
@@ -106,27 +100,26 @@ async def _(fry):
             await fry.edit("`Mohon Matikan Setelan Privasi Forward...`")
         else:
             downloaded_file_name = await fry.client.download_media(
-                response.media,
-                TEMP_DOWNLOAD_DIRECTORY
+                response.media, TEMP_DOWNLOAD_DIRECTORY
             )
             await fry.client.send_file(
                 fry.chat_id,
                 downloaded_file_name,
                 force_document=False,
-                reply_to=message_id_to_reply
+                reply_to=message_id_to_reply,
             )
             """ - cleanup chat after completed - """
             try:
                 msg_level
             except NameError:
-                await fry.client.delete_messages(conv.chat_id,
-                                                 [msg.id, response.id])
+                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
             else:
                 await fry.client.delete_messages(
-                    conv.chat_id,
-                    [msg.id, response.id, r.id, msg_level.id])
+                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
+                )
     await fry.delete()
     return os.remove(downloaded_file_name)
+
 
 """
 CMD_HELP.update({
