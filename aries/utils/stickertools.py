a114aea208db2c09ad0637378e0e48cd448e59bf
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-23 10:22:38 +0700

    Create stickertools.py

---
+++ aries/utils/stickertools.py
@@ -0,0 +1,37 @@
+import io
+import os
+import random
+import re
+from os import remove
+
+import cv2
+import numpy as np
+
+
+from PIL import Image, ImageDraw
+from telethon.errors import YouBlockedUserError
+from telethon.tl.functions.stickers import SuggestShortNameRequest
+from telethon.tl.types import DocumentAttributeFilename, DocumentAttributeSticker
+from telethon.utils import get_input_document
+
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+",
+)
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, "", inputString)
