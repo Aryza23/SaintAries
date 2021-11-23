94fd5cd0b3e81c843678a5e8772f85c897f0789b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-23 09:15:44 +0700

    Update aries.py

--- aries/modules/aries.py
+++ aries/modules/aries.py
@@ -1,4 +1,8 @@
 import random
+import requests
+from bs4 import BeautifulSoup as bs
+from pyjokes import get_joke
+from telethon.errors import ChatSendMediaForbiddenError
 
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
@@ -6,6 +10,57 @@ from telegram.ext import CallbackContext
 import aries.modules.aries_strings as aries_strings
 from aries import dispatcher
 from aries.modules.disable import DisableAbleCommandHandler
+from aries.events import register
+
+
+@register(pattern="^/joke ?(.*)")
+async def joke(event):
+    await event.reply(get_joke())
+
+
+@register(pattern="^/insult ?(.*)")
+async def insult(event):
+    m = await event.reply("Generating...")
+    nl = "https://fungenerators.com/random/insult/new-age-insult/"
+    ct = requests.get(nl).content
+    bsc = bs(ct, "html.parser", from_encoding="utf-8")
+    cm = bsc.find_all("h2")[0].text
+    await m.edit(f"{cm}")
+
+
+@register(pattern="url ?(.*)")
+async def url(event):
+    input_str = event.pattern_match.group(1)
+    if not input_str:
+        await event.reply("Give some url")
+        return
+    sample_url = "https://da.gd/s?url={}".format(input_str)
+    response_api = requests.get(sample_url).text
+    if response_api:
+        await event.reply(           
+            "**Shortened url**==> {}\n**Given url**==> {}.".format(
+                response_api, input_str
+            ),
+        )
+    else:
+        await event.reply("`Something went wrong. Please try again Later.`")
+
+
+@register(pattern="^/xo ?(.*)")
+async def xo(event):
+    xox = await event.client.inline_query("xobot", "play")
+    await xox[random.randrange(0, len(xox) - 1)].click(
+        ult.chat_id, reply_to=ult.reply_to_msg_id, silent=True, hide_via=True
+    )
+    await event.delete()
+
+@register(pattern="^/wordi ?(.*)")
+async def word(event):
+    game = await event.client.inline_query("wordibot", "play")
+    await game[0].click(
+        ult.chat_id, reply_to=event.reply_to_msg_id, silent=True, hide_via=True
+    )
+    await event.delete()
 
 AD_STRINGS = (
     "_*kembali dengan versi terbaik, karna di sini aku masih menunggumu,masih tentang kamu*_",
@@ -41,7 +96,13 @@ __help__ = """
  ❍ `/diaryaryza`*:* Check Aja.
  ❍ `/apod`*:* Get Astronomy Picture of Day by NASA.
  ❍ `/devian` <search query> ; <no of pics> *:* Devian-Art Image Search.
+ ❍ `/joke`*:* To get random joke.
+ ❍ `/inslut`*:* Insult someone..
+ ❍ `/url <long url>`*:* To get a shorten link of long link.
+ ❍ `/xo`*:* Opens tic tac game only where using inline mode is allowed.
+ ❍ `/wordi`*:* Opens word game only where using inline mode is allowed.
 """
+
 
 ARIES_HANDLER = DisableAbleCommandHandler("aries", aries, run_async=True)
 dispatcher.add_handler(ARIES_HANDLER)
