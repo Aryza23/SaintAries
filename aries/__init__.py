b83a612e6488268f2ab7b5d1696878faecda173d
idzero23 <idzero23@users.noreply.github.com>
2021-11-25 09:28:12 +0000

    Automated code formatting
    Signed-off-by: idzero23 <88209926+idzero23@users.noreply.github.com>

--- aries/__init__.py
+++ aries/__init__.py
@@ -313,10 +313,12 @@ async def get_entity(client, entity):
                 entity_client = kp
     return entity, entity_client
 
+
 async def eor(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
