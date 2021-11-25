0d751c84da40d7ecf853c628bd841d86bee92902
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-25 18:14:29 +0700

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -5,7 +5,6 @@ import os
 import re
 import sys
 import time
-import psycopg2
 
 import spamwatch
 import telegram.ext as tg
@@ -92,7 +91,6 @@ if ENV:
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     DB_URI = os.environ.get("DATABASE_URL")
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     DB_URI = DB_URI.replace(
         "postgres://", "postgresql://", 1
     )  # rest of connection code using the connection string `uri`
