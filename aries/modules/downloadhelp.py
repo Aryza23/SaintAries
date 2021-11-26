d89821cbcba27097def91762978f77061d34e26f
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-26 20:27:44 +0700

    Create downloadhelp.py

---
+++ aries/modules/downloadhelp.py
@@ -0,0 +1,13 @@
+__mod_help__ = """
+I can help you download files less than 2GB.
+Just send me a link and reply it with /dl.
+
+ ❍ `/dl`<link>or<reply link>*:* for download.
+ ❍ `/aboutme`*:* To know More About me.
+ ❍ `/support`*:* to get support.
+ ❍ `/donate`*:* to donate this bot !
+"""
+
+__mod_name__ = "Link To Download"
+
+__command_list__ = ["dl" "aboutme" "support" "donate"]
