bf0eb1287130337067eb4e7159a2b55f6e68c4c4
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-29 19:00:38 +0700

    Update language.py

--- aries/langs/language.py
+++ aries/langs/language.py
@@ -23,7 +23,7 @@ class Language:
             if filename.endswith(".yaml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
-                    open(r"./tg_bot/langs/" + filename, encoding="utf8")
+                    open(r"./aries/langs/" + filename, encoding="utf8")
                 )
 
     def get_languages(self) -> Dict:
