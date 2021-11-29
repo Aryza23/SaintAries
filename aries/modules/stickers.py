856084bf15ed4ecdaffeafd2177eca09ed8c1305
idzero23 <idzero23@users.noreply.github.com>
2021-11-29 15:08:35 +0000

    Automated code formatting
    Signed-off-by: idzero23 <88209926+idzero23@users.noreply.github.com>

--- aries/modules/stickers.py
+++ aries/modules/stickers.py
@@ -211,22 +211,22 @@ def kang(update: Update, context: CallbackContext):
                         emojis=sticker_emoji,
                     )
                     msg.reply_text(
-                    f"Kang Sticker successfully Click this button to add in your pack. "
-                    + f"\nEmoji is: {sticker_emoji}",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
+                        f"Kang Sticker successfully Click this button to add in your pack. "
+                        + f"\nEmoji is: {sticker_emoji}",
+                        reply_markup=InlineKeyboardMarkup(
                             [
-                                InlineKeyboardButton(
-                                    "Add To Your Pack",
-                                    url=f"t.me/addstickers/{packname}",
-                                ),
-                                InlineKeyboardButton(
-                                    "[❌]",
-                                    callback_data="close",
-                                ),
+                                [
+                                    InlineKeyboardButton(
+                                        "Add To Your Pack",
+                                        url=f"t.me/addstickers/{packname}",
+                                    ),
+                                    InlineKeyboardButton(
+                                        "[❌]",
+                                        callback_data="close",
+                                    ),
+                                ]
                             ]
-                        ]
-                    ),
+                        ),
                         parse_mode=ParseMode.MARKDOWN,
                     )
                 elif e.message == "Stickers_too_much":
@@ -339,22 +339,22 @@ def kang(update: Update, context: CallbackContext):
                 emojis=sticker_emoji,
             )
             msg.reply_text(
-                    f"Kang Sticker successfully Click this button to add in your pack. "
-                    + f"\nEmoji is: {sticker_emoji}",
-                    reply_markup=InlineKeyboardMarkup(
+                f"Kang Sticker successfully Click this button to add in your pack. "
+                + f"\nEmoji is: {sticker_emoji}",
+                reply_markup=InlineKeyboardMarkup(
+                    [
                         [
-                            [
-                                InlineKeyboardButton(
-                                    "Add To Your Pack",
-                                    url=f"t.me/addstickers/{packname}",
-                                ),
-                                InlineKeyboardButton(
-                                    "[❌]",
-                                    callback_data="close",
-                                ),
-                            ]
+                            InlineKeyboardButton(
+                                "Add To Your Pack",
+                                url=f"t.me/addstickers/{packname}",
+                            ),
+                            InlineKeyboardButton(
+                                "[❌]",
+                                callback_data="close",
+                            ),
                         ]
-                    ),
+                    ]
+                ),
                 parse_mode=ParseMode.MARKDOWN,
             )
         except OSError as e:
