9e65677b5116ed43c9ad27a30344fb77725e2944
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-12-13 08:21:59 +0700

    Update contributers.py

--- aries/modules/contributers.py
+++ aries/modules/contributers.py
@@ -1,6 +1,16 @@
 import github
 from pyrogram import filters
 from aries import pbot as app
+from platform import python_version as y
+from telegram import __version__ as o
+from pyrogram import __version__ as z
+from telethon import __version__ as s
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
+
+
+IDZ = "https://telegra.ph/file/08f41dc969408c08b8c68.jpg"
+
 
 
 @app.on_message(filters.command("repo") & ~filters.edited)
@@ -11,13 +21,36 @@ async def give_repo(c, m):
     repo = g.get_repo("idzero23/SaintAries")
     for i in repo.get_contributors():
         count += 1
-        list_of_users += f"*{count}.* [{i.login}](https://github.com/{i.login})\n"
-    text = f"""[Github](https://github.com/idzero23/SaintAries) | [support group](https://t.me/idzeroidsupport)
+        list_of_users += f"•{count}. [{i.login}](https://github.com/{i.login})\n"
+    await m.reply_photo(
+        photo=IDZ,
+        caption=f"""**Hey I'm Aries Robot** 
+**Owner repo : [Aryza](https://t.me/idzxartez)**
+**Python Version :** `{y()}`
+**Library Version :** `{o}`
+**Telethon Version :** `{s}`
+**Pyrogram Version :** `{z}`
+
 ```----------------
-| Contributors |
+| **Collaborators** |
 ----------------```
-{list_of_users}"""
-    await m.reply(text, disable_web_page_preview=False)
+{list_of_users}
+**Create your own with click button bellow.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Repo", url="https://github.com/idzero23/SaintAries"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton("Support", url="https://t.me/idzeroidsupport"),
+                    InlineKeyboardButton("Update", url="https://t.me/idzeroid"),
+                ],
+            ]
+        ),
+    )
 
 
 __mod_name__ = "REPO"
